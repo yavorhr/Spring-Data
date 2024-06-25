e4e8c00668b0147becbb87f15ab2247bd7f03ded
wolfie <yavorhristozov@gmail.com>
2024-06-25 17:07:22 +0300

    add : demo queries to the Database

--- 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
+++ 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
@@ -1,3 +1,4 @@
+import entity.User;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
@@ -12,7 +13,25 @@ public class Main {
 
     Transaction transaction = session.beginTransaction();
 
+    //create user
+    var user = new User();
+    user.setUsername("Pesho");
+    user.setPassword("Password");
+    session.save(user);
+
+    // getById
+    var userPesho = session.get(User.class, 1);
+    System.out.println(user1.getId());
+
+    //delete user
+    session.delete(userPesho);
+
+    //getById and rename user
+    var peshoUser = session.get(User.class,1);
+    session.update(peshoUser);
+
     transaction.commit();
+
   }
 }
 

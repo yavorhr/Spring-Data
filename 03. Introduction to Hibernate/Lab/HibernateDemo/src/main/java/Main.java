321b3882ad9ef572d57277759c9bdf5ebdadfdf0
wolfie <yavorhristozov@gmail.com>
2024-06-25 14:15:04 +0300

    add : Open session to MySQL using Hibernate

--- 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
+++ 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
@@ -1,7 +1,16 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
   public static void main(String[] args) {
 
+    Session session = new Configuration().configure().buildSessionFactory().openSession();
+    Transaction transaction = session.beginTransaction();
+
+    transaction.commit();
   }
 }
+
+
+

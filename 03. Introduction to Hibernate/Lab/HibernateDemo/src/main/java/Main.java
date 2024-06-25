5e208c26ab72d81f4e3f6e33c854f0e125cf862f
wolfie <yavorhristozov@gmail.com>
2024-06-25 16:41:23 +0300

    add : User.hbm.xml file

--- 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
+++ 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
@@ -5,7 +5,11 @@ import org.hibernate.cfg.Configuration;
 public class Main {
   public static void main(String[] args) {
 
-    Session session = new Configuration().configure().buildSessionFactory().openSession();
+    Session session = new Configuration()
+            .configure()
+            .buildSessionFactory()
+            .openSession();
+
     Transaction transaction = session.beginTransaction();
 
     transaction.commit();

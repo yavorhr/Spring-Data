4b2f3408d060d3c725edad210eb711e170c556be
wolfie <yavorhristozov@gmail.com>
2024-06-26 12:51:37 +0300

    add : demo for JPQL query

--- 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
+++ 03. Introduction to Hibernate/Lab/HibernateDemo/src/main/java/Main.java
@@ -1,37 +1,45 @@
+
 import entity.User;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
 
-    Session session = new Configuration()
+    SessionFactory sessionFactory = new Configuration()
             .configure()
-            .buildSessionFactory()
-            .openSession();
+            .buildSessionFactory();
 
-    Transaction transaction = session.beginTransaction();
+    EntityManager session = sessionFactory.openSession();
 
-    //create user
-    var user = new User();
-    user.setUsername("Pesho");
-    user.setPassword("Password");
-    session.save(user);
+    TypedQuery<User> query = session.createQuery("SELECT u FROM User u where u.username = :un", User.class);
+    query.setParameter("un", "pesho");
+    List<User> resultList = query.getResultList();
 
-    // getById
-    var userPesho = session.get(User.class, 1);
-    System.out.println(user1.getId());
-
-    //delete user
-    session.delete(userPesho);
-
-    //getById and rename user
-    var peshoUser = session.get(User.class,1);
-    session.update(peshoUser);
-
+    resultList.forEach(u -> System.out.println(u.getUsername()));
+//    //create user
+//    var user = new User();
+//    user.setUsername("Pesho");
+//    user.setPassword("Password");
+//    session.save(user);
+//
+//    // getById
+//    var userPesho = session.get(User.class, 1);
+//    System.out.println(userPesho.getId());
+//
+//    //delete user
+//    session.delete(userPesho);
+//
+//    //getById and rename user
+//    var peshoUser = session.get(User.class,1);
+//    session.update(peshoUser);
+//
     transaction.commit();
-
   }
 }
 

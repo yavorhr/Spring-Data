68b6f02a9997f75f4633f2b57498dcf425ea864c
wolfie <yavorhristozov@gmail.com>
2024-07-04 14:08:47 +0300

    add : init entitiyManagerFactory and entityManager

--- 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Main.java
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Main.java
@@ -1,4 +1,13 @@
 package entities;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
+  public static void main(String[] args) {
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("code_first_exercise");
+    EntityManager em = emf.createEntityManager();
+
+  }
 }

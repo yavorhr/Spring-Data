c348460fe99b8d79b293a2d9d88809a4bb446503
wolfie <yavorhristozov@gmail.com>
2024-07-04 13:13:35 +0300

    add : Truck class

---
+++ 04. Hibernate Code First/Lab/codeFirst_Lab/src/main/java/JOIN_TABLE/entities/Truck.java
@@ -0,0 +1,29 @@
+package JOIN_TABLE.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Truck extends TransportationVehicle {
+  private final static String type = "TRUCK";
+  private int numOfContainers;
+
+  public Truck(){
+  }
+
+  public Truck(String type, int numOfContainers,int loadCapacity) {
+    super(type,loadCapacity);
+    this.numOfContainers = numOfContainers; }
+
+  @Override
+  public static String getType() {
+    return this.type;
+  }
+
+  public int getNumOfContainers() {
+    return numOfContainers;
+  }
+
+  public void setNumOfContainers(int numOfContainers) {
+    this.numOfContainers = numOfContainers;
+  }
+}

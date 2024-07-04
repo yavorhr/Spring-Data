ab9536c5c9ccec5c847617feaa4575589d9e54fd
wolfie <yavorhristozov@gmail.com>
2024-07-04 16:26:40 +0300

    add : Sales class
    02. Sales Database

---
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Sale.java
@@ -0,0 +1,54 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class Sale extends BaseEntity {
+  private Product product;
+  private Customer customer;
+  private StoreLocation storeLocation;
+  private LocalDateTime date;
+
+  public Sale() {
+  }
+
+  // many Sales for one product
+  @ManyToOne
+  public Product getProduct() {
+    return product;
+  }
+
+  public void setProduct(Product product) {
+    this.product = product;
+  }
+
+  // many sales per one customer
+  @ManyToOne
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  // many sales in one store
+  @ManyToOne
+  public StoreLocation getStoreLocation() {
+    return storeLocation;
+  }
+
+  public void setStoreLocation(StoreLocation storeLocation) {
+    this.storeLocation = storeLocation;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDateTime date) {
+    this.date = date;
+  }
+}

962cbfc50a4ba86e582336db9d93ee900eda3b32
wolfie <yavorhristozov@gmail.com>
2024-07-04 16:12:54 +0300

    add : 02. Sales Database
    add Customer class

---
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Customer.java
@@ -0,0 +1,41 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Customer extends BaseEntity {
+  private String name;
+  private String email;
+  private String creditCardNumber;
+
+  public Customer() {
+  }
+
+  @Column(name = "name",nullable = false)
+  public String getName() {
+    return name;
+  }
+
+  @Column(name = "email", unique = true, nullable = false)
+  public String getEmail() {
+    return email;
+  }
+
+  @Column(name = "credit_card_number", unique = true, nullable = false)
+  public String getCreditCardNumber() {
+    return creditCardNumber;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setCreditCardNumber(String creditCardNumber) {
+    this.creditCardNumber = creditCardNumber;
+  }
+}

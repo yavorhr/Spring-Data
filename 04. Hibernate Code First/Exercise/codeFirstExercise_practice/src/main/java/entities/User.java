c2e0258eefec0583b142e9dbadb6f9cf4ccca232
wolfie <yavorhristozov@gmail.com>
2024-07-05 13:25:18 +0300

    add : User class
    03. University System

---
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/User.java
@@ -0,0 +1,44 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class User extends BaseEntity {
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+
+  public User() {
+  }
+
+  @Column(name = "first_name", nullable = false)
+  public String getFirstName() {
+    return firstName;
+  }
+
+  @Column(name = "last_name", nullable = false)
+  public String getLastName() {
+    return lastName;
+  }
+
+  @Column(name = "phone_number")
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+}

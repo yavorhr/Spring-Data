66df161d054ddbf9ee8a624675d34a00195f45e7
wolfie <yavorhristozov@gmail.com>
2024-07-05 14:59:10 +0300

    add : Visitation class

--- 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Visitation.java
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Visitation.java
@@ -1,9 +1,45 @@
 package entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "visitations")
-public class Visitation {
+public class Visitation extends BaseEntity {
+  private LocalDateTime dateAndTime;
+  private String comment;
+  private Patient patient;
+
+  public Visitation() {
+  }
+
+  @ManyToOne
+  public Patient getPatient() {
+    return this.patient;
+  }
+
+  @Column(name = "date_and_time", nullable = false)
+  public LocalDateTime getDateAndTime() {
+    return this.dateAndTime;
+  }
+
+  @Column(name = "comment", columnDefinition = "TEXT")
+  public String getComment() {
+    return this.comment;
+  }
+
+  public void setPatient(Patient patient) {
+    this.patient = patient;
+  }
+
+  public void setDateAndTime(LocalDateTime dateAndTime) {
+    this.dateAndTime = dateAndTime;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
 }

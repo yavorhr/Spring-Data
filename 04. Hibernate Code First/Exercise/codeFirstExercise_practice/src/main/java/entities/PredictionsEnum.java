4c7a989f9af54a1dc4213306973342d82ae299fe
wolfie <yavorhristozov@gmail.com>
2024-07-06 17:04:28 +0300

    add : PredictionsEnum

---
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/PredictionsEnum.java
@@ -0,0 +1,22 @@
+package entities;
+
+public enum PredictionsEnum {
+  HOME_TEAM_WIN("Home team win"),
+  DRAW_GAME("Draw Game"),
+  AWAY_TEAM_WIN("Away Team Win");
+
+  private final String displayPrediction;
+
+  PredictionsEnum(String displayPrediction) {
+    this.displayPrediction = displayPrediction;
+  }
+
+  @Override
+  public String toString() {
+    return this.displayPrediction;
+  }
+
+  public String getDisplayPrediction() {
+    return this.displayPrediction;
+  }
+}

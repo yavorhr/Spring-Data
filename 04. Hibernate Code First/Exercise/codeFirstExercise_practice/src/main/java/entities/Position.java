3364162e25ab51a36ade72d91d661161f739ad4b
wolfie <yavorhristozov@gmail.com>
2024-07-06 16:51:02 +0300

    add : Position entity

---
+++ 04. Hibernate Code First/Exercise/codeFirstExercise_practice/src/main/java/entities/Position.java
@@ -0,0 +1,23 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "positions")
+public class Position extends BaseEntity {
+  private String positionDescription;
+
+  public Position() {
+  }
+
+  @Column(name = "position_description")
+  public String getPositionDescription() {
+    return this.positionDescription;
+  }
+
+  public void setPositionDescription(String positionDescription) {
+    this.positionDescription = positionDescription;
+  }
+}

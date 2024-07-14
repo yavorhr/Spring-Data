da097080bd4674e4e42692ba938fb007ccf1e2fe
wolfie <yavorhristozov@gmail.com>
2024-07-14 14:50:40 +0300

    add : Spring init project

---
+++ 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/SpringintroPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springintro_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringintroPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringintroPracticeApplication.class, args);
+  }
+
+}
---
+++ 05. Spring Data Intro/Exercise/springintro_practice/src/test/java/com/example/springintro_practice/SpringintroPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintro_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringintroPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

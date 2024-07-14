6749406689e432e139d543803dab8e7bbd977295
wolfie <yavorhristozov@gmail.com>
2024-07-14 14:44:54 +0300

    refact : project structure

---
+++ 05. Spring Data Intro/Exercise/spring_intro_solved/src/main/java/com/example/springintro/service/AuthorService.java
@@ -0,0 +1,15 @@
+package com.example.springintro.service;
+
+import com.example.springintro.model.entity.Author;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface AuthorService {
+    void seedAuthors() throws IOException;
+
+    Author getRandomAuthor();
+
+    List<String> getAllAuthorsOrderByCountOfTheirBooks();
+
+}

31bb3a9b8e1331c57a4c32e1c687dad62c165500
wolfie <yavorhristozov@gmail.com>
2024-07-14 15:29:18 +0300

    add : Author entity

--- 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/model/Author.java
+++ 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/model/Author.java
@@ -1,9 +1,39 @@
 package com.example.springintro_practice.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.Columns;
 
 @Entity
 @Table(name = "authors")
-public class Author {
+public class Author extends BaseEntity {
+  private String firstName;
+  private String lastName;
+
+  public Author() {
+  }
+
+  public Author(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  @Column(name = "first_name")
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  @Column(name = "last_name", nullable = false)
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }

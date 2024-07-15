9f9a3204e0e29cd3e2f45464d5ad131fef431f14
wolfie <yavorhristozov@gmail.com>
2024-07-15 12:32:57 +0300

    add : getRandomAuthor()
    AuthorServiceImpl

--- 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/service/AuthorService.java
+++ 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/service/AuthorService.java
@@ -1,9 +1,12 @@
 package com.example.springintro_practice.service;
 
+import com.example.springintro_practice.model.Author;
+
 import java.io.IOException;
 
 public interface AuthorService {
 
   void seedAuthors() throws IOException;
 
+  Author getRandomAuthor();
 }
--- 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/service/impl/AuthorServiceImpl.java
+++ 05. Spring Data Intro/Exercise/springintro_practice/src/main/java/com/example/springintro_practice/service/impl/AuthorServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
@@ -27,6 +28,13 @@ public class AuthorServiceImpl implements AuthorService {
     Files.readAllLines(Path.of(FilePath.AUTHORS_FILE_PATH))
             .forEach(this::saveAuthorToDatabase);
 
+  }
+
+  @Override
+  public Author getRandomAuthor() {
+    long randomId = ThreadLocalRandom.current().nextLong(1, this.authorRepository.count() + 1);
+
+    return authorRepository.findById(randomId).orElse(null);
   }
 
 

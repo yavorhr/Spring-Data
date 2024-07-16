e552f71c3846871d6f1ad6bf64e6b852a85dbc5b
wolfie <yavorhristozov@gmail.com>
2024-07-16 15:38:43 +0300

    add : CommandLineRunnerImpl

---
+++ 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/CommandLineRunnerImpl.java
@@ -0,0 +1,14 @@
+package com.example.advquerying;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("sssssssss");
+  }
+}

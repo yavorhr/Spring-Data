7edbc7518bb13c2332f1b8b3ba8d177c29ee0367
wolfie <yavorhristozov@gmail.com>
2024-07-16 15:42:51 +0300

    add : ShampooService & ShampooServiceImpl

---
+++ 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/services/ShampooService.java
@@ -0,0 +1,5 @@
+package com.example.advquerying.services;
+
+public interface ShampooService {
+  
+}
---
+++ 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/services/impl/ShampooServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.advquerying.services.impl;
+
+import com.example.advquerying.services.ShampooService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShampooServiceImpl implements ShampooService {
+}

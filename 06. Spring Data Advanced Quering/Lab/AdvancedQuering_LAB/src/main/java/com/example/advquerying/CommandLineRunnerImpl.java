0f7da952213beb84f7379ef0499a501dd12cf128
wolfie <yavorhristozov@gmail.com>
2024-07-16 15:46:36 +0300

    add : CLR console i/o

--- 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/CommandLineRunnerImpl.java
+++ 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/CommandLineRunnerImpl.java
@@ -1,14 +1,35 @@
 package com.example.advquerying;
 
+import com.example.advquerying.services.ShampooService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Scanner;
+
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+  Scanner scanner = new Scanner(System.in);
+  private final ShampooService shampooService;
 
+  public CommandLineRunnerImpl(ShampooService shampooService) {
+    this.shampooService = shampooService;
+  }
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("sssssssss");
+    System.out.println("Please enter task number:");
+    int taskNumber = Integer.parseInt(scanner.nextLine());
+
+    switch (taskNumber) {
+      case 1 -> selectShampooBySize();
+    }
+  }
+
+  private void selectShampooBySize() {
+    System.out.println("Please enter shampoo size :");
+    String size = scanner.nextLine();
+
+    List<String> shampoos = this.shampooService.findAllShampoosBySize(size);
   }
 }
--- 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/services/ShampooService.java
+++ 06. Spring Data Advanced Quering/Lab/AdvancedQuering_LAB/src/main/java/com/example/advquerying/services/ShampooService.java
@@ -1,5 +1,5 @@
 package com.example.advquerying.services;
 
 public interface ShampooService {
-  
+
 }

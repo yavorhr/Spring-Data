879569667c5ca7277db387cefdfc950feeaa5b10
wolfie <yavorhristozov@gmail.com>
2024-07-17 16:38:32 +0300

    add : i/o structure in CLR
    cases 90 - 93 are from the old lab exercise

--- 06. Spring Data Advanced Quering/Exercise/advancedQueryingPractice/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
+++ 06. Spring Data Advanced Quering/Exercise/advancedQueryingPractice/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -6,6 +6,8 @@ import com.example.springintro.service.BookService;
 import com.example.springintro.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 
 @Component
@@ -13,21 +15,29 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
     private final CategoryService categoryService;
     private final AuthorService authorService;
     private final BookService bookService;
+    private final BufferedReader bufferedReader;
 
-    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService) {
+    public CommandLineRunnerImpl(CategoryService categoryService, AuthorService authorService, BookService bookService, BufferedReader bufferedReader) {
         this.categoryService = categoryService;
         this.authorService = authorService;
         this.bookService = bookService;
+        this.bufferedReader = bufferedReader;
     }
 
     @Override
     public void run(String... args) throws Exception {
         seedData();
 
-        //printAllBooksAfterYear(2000);
-//        printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
-     //   printAllAuthorsAndNumberOfTheirBooks();
-//        pritnALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+        System.out.println("Please enter exercise number: ");
+        int number = Integer.parseInt(this.bufferedReader.readLine());
+
+        switch (number){
+            case 90 -> printAllBooksAfterYear(2000);
+            case 91 -> printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
+            case 92 -> printAllAuthorsAndNumberOfTheirBooks();
+            case 93 -> printALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+        }
+
 
     }
 

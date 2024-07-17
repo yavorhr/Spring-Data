46b1ce9c5909c3ed50e473bca8f9f48fbfb16cee
wolfie <yavorhristozov@gmail.com>
2024-07-17 16:34:54 +0300

    add : ApplicationConfiguration file
    add @Bean for BufferedReader

--- 06. Spring Data Advanced Quering/Exercise/advancedQueryingPractice/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
+++ 06. Spring Data Advanced Quering/Exercise/advancedQueryingPractice/src/main/java/com/example/springintro/CommandLineRunnerImpl.java
@@ -6,12 +6,10 @@ import com.example.springintro.service.BookService;
 import com.example.springintro.service.CategoryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
-
     private final CategoryService categoryService;
     private final AuthorService authorService;
     private final BookService bookService;
@@ -29,11 +27,11 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
         //printAllBooksAfterYear(2000);
 //        printAllAuthorsNamesWithBooksWithReleaseDateBeforeYear(1990);
      //   printAllAuthorsAndNumberOfTheirBooks();
-        pritnALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
+//        pritnALlBooksByAuthorNameOrderByReleaseDate("George", "Powell");
 
     }
 
-    private void pritnALlBooksByAuthorNameOrderByReleaseDate(String firstName, String lastName) {
+    private void printALlBooksByAuthorNameOrderByReleaseDate(String firstName, String lastName) {
         bookService
                 .findAllBooksByAuthorFirstAndLastNameOrderByReleaseDate(firstName, lastName)
                 .forEach(System.out::println);

a9bbb47b70965866afb86d85a8c83ce82e19ac30
wolfie <yavorhristozov@gmail.com>
2024-07-20 15:07:13 +0300

    add : new fields to Employee
    Manager, isOnHoldiay, subordinates

--- 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
+++ 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
@@ -1,11 +1,19 @@
 package com.example.mappingobjectslab.entity.dto;
 
+import com.example.mappingobjectslab.entity.model.Employee;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
 import java.math.BigDecimal;
+import java.util.List;
 
 public class EmployeeDto {
   private String firstName;
   private String lastName;
   private BigDecimal salary;
+  private boolean isOnHoliday;
+  private Employee manager;
+  private List<Employee> subordinates;
 
   public EmployeeDto() {
   }
@@ -22,6 +30,20 @@ public class EmployeeDto {
     return salary;
   }
 
+  public boolean isOnHoliday() {
+    return isOnHoliday;
+  }
+
+  @ManyToOne
+  public Employee getManager() {
+    return manager;
+  }
+
+  @OneToMany(mappedBy = "manager")
+  public List<Employee> getSubordinates() {
+    return subordinates;
+  }
+
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
@@ -32,5 +54,9 @@ public class EmployeeDto {
 
   public void setSalary(BigDecimal salary) {
     this.salary = salary;
+  }
+
+  public void setOnHoliday(boolean onHoliday) {
+    isOnHoliday = onHoliday;
   }
 }

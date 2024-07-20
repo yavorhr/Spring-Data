c0ff6b861a44bf45649f5de607fcb7c69ca5f964
wolfie <yavorhristozov@gmail.com>
2024-07-20 14:45:29 +0300

    add : 01. Simple Mapping

--- 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
+++ 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
@@ -1,15 +1,41 @@
 package com.example.mappingobjectslab;
 
+import com.example.mappingobjectslab.entity.dto.EmployeeDto;
+import com.example.mappingobjectslab.services.EmployeeService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+  private final Scanner scanner;
+  private final EmployeeService employeeService;
+
+  public CommandLineRunnerImpl(Scanner scanner, EmployeeService employeeService) {
+    this.scanner = scanner;
+    this.employeeService = employeeService;
+  }
 
   @Override
   public void run(String... args) throws Exception {
-
     System.out.println("Please select task number: ");
-    
+    int taskNumber = Integer.parseInt(scanner.nextLine());
+
+    switch (taskNumber) {
+      case 1 -> simpleMapping();
+    }
+
+  }
+
+  private void simpleMapping() {
+    System.out.println("Please select employee id: ");
+    int employeeId = Integer.parseInt(scanner.nextLine());
+
+    EmployeeDto employeeDto = this.employeeService.findEmployeeById(employeeId);
+
+    System.out.printf("%s %s %.2f\n", employeeDto.getFirstName(),
+            employeeDto.getLastName(),
+            employeeDto.getSalary());
   }
 }
---
+++ 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/ApplicationBeanConf.java
@@ -0,0 +1,15 @@
+package com.example.mappingobjectslab.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Scanner;
+
+@Configuration
+public class ApplicationBeanConf {
+
+  @Bean
+  public Scanner scanner(){
+    return new Scanner(System.in);
+  }
+}

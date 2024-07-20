b27da9f5367250f7bcd9727225937871e5642a5f
wolfie <yavorhristozov@gmail.com>
2024-07-20 15:19:13 +0300

    refact : EmployeeDto

--- 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
+++ 07. Spring Data Auto Mapping Objects/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
@@ -1,41 +1,14 @@
 package com.example.mappingobjectslab.entity.dto;
-
-import com.example.mappingobjectslab.entity.model.Employee;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-
 import java.math.BigDecimal;
-import java.util.List;
 
-public class EmployeeDto {
-  private String firstName;
-  private String lastName;
+public class EmployeeDto extends BasicDto {
   private BigDecimal salary;
-
-
+  
   public EmployeeDto() {
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
   }
 
   public BigDecimal getSalary() {
     return salary;
-  }
-
-
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
   }
 
   public void setSalary(BigDecimal salary) {

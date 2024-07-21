6e877bb25e6f2902f0ae23a53eaa748d5ed37b16
wolfie <yavorhristozov@gmail.com>
2024-07-21 11:52:32 +0300

    init : Spring project

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise_practice/src/main/java/com/example/dto_exercise_practice/DtoExercisePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dto_exercise_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoExercisePracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DtoExercisePracticeApplication.class, args);
+  }
+
+}

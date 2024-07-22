1972fc5793e0712cc79f8e3bdd0639730d72715c
wolfie <yavorhristozov@gmail.com>
2024-07-22 15:36:23 +0300

    add : UserRepository

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise_practice/src/main/java/com/example/dto_exercise_practice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.dto_exercise_practice.repository;
+
+import com.example.dto_exercise_practice.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
--- 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise_practice/src/main/java/com/example/dto_exercise_practice/service/impl/UserServiceImpl.java
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise_practice/src/main/java/com/example/dto_exercise_practice/service/impl/UserServiceImpl.java
@@ -1,20 +1,42 @@
 package com.example.dto_exercise_practice.service.impl;
 
 import com.example.dto_exercise_practice.model.dto.RegisterUserDto;
+import com.example.dto_exercise_practice.model.entity.User;
 import com.example.dto_exercise_practice.service.UserService;
+import com.example.dto_exercise_practice.util.ValidationUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintViolation;
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
   private final ModelMapper mapper;
+  private final ValidationUtil validationUtil;
 
-  public UserServiceImpl(ModelMapper mapper) {
+  public UserServiceImpl(ModelMapper mapper, ValidationUtil validationUtil) {
     this.mapper = mapper;
+    this.validationUtil = validationUtil;
   }
 
   @Override
   public void registerUser(RegisterUserDto registerUserDto) {
+    if (!registerUserDto.getPassword().equals(registerUserDto.getConfirmPassword())) {
+      System.out.println("Password does not match!");
+      return;
+    }
+
+    Set<ConstraintViolation<RegisterUserDto>> violation = this.validationUtil.violation(registerUserDto);
+    if (!violation.isEmpty()) {
+      violation
+              .stream()
+              .map(ConstraintViolation::getMessage)
+              .forEach(System.out::println);
+      return;
+    }
+
+    User user = this.mapper.map(registerUserDto, User.class);
 
   }
 }

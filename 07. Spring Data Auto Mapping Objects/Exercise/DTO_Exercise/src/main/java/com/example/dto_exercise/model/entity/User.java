fd5959d3fba05b1ae92c019a0f487d5f6bf6c383
wolfie <yavorhristozov@gmail.com>
2024-07-22 16:10:12 +0300

    add : Game entity

--- 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/model/entity/User.java
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/model/entity/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {

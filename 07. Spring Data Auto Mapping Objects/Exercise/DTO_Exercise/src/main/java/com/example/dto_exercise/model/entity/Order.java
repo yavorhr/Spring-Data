85c19128aa35412c6d92fc88e93adc793323c9ac
wolfie <yavorhristozov@gmail.com>
2024-07-22 16:10:39 +0300

    add : Order entity

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/model/entity/Order.java
@@ -0,0 +1,36 @@
+package com.example.dto_exercise.model.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+    private User buyer;
+    private Set<Game> games;
+
+    public Order() {
+    }
+
+    @ManyToOne
+    public User getBuyer() {
+        return buyer;
+    }
+
+    @ManyToMany
+    public Set<Game> getGames() {
+        return games;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public void setGames(Set<Game> games) {
+        this.games = games;
+    }
+}

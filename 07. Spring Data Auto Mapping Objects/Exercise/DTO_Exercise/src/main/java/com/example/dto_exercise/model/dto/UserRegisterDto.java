6585bcd0df7f7d0d86e81cf9b6d09c0806c5a9ee
wolfie <yavorhristozov@gmail.com>
2024-07-22 16:04:10 +0300

    add : UserRegisterDto

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/model/dto/UserRegisterDto.java
@@ -0,0 +1,42 @@
+package com.example.dto_exercise.model.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class UserRegisterDto {
+    private String email;
+    private String password;
+    private String confirmedPassword;
+    private String fullName;
+
+    public UserRegisterDto() {
+    }
+
+    public UserRegisterDto(String email, String password, String confirmedPassword, String fullName) {
+        this.email = email;
+        this.password = password;
+        this.confirmedPassword = confirmedPassword;
+        this.fullName = fullName;
+    }
+
+    @Email(message = "Enter valid email")
+    public String getEmail() {
+        return email;
+    }
+
+    @Pattern(regexp = "[A-Za-z\\d]{6,}", message = "Enter valid password")
+    public String getPassword() {
+        return password;
+    }
+
+
+    @Size(min = 2)
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getConfirmedPassword() {
+        return confirmedPassword;
+    }
+}

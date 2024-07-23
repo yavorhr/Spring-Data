b34c8a8b92daf1244b3d558f65d3739cf350fdc6
wolfie <yavorhristozov@gmail.com>
2024-07-23 15:33:23 +0300

    update

--- 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/config/ApplicationBeanConfiguration.java
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/config/ApplicationBeanConfiguration.java
@@ -3,17 +3,11 @@ package com.example.dto_exercise.config;
 
 import com.example.dto_exercise.model.dto.GameAddDto;
 import com.example.dto_exercise.model.entity.Game;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationBeanConfiguration {

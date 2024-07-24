bab8b7d64889a084e1d109e735ec355dbc928b72
wolfie <yavorhristozov@gmail.com>
2024-07-24 19:35:44 +0300

    refact : CLR

--- 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
+++ 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
@@ -24,67 +24,19 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("Please select task number: ");
-    int taskNumber = Integer.parseInt(scanner.nextLine());
+    System.out.println("Please insert command: ");
+    String[] tokens = scanner.nextLine().split(" ");
+    String command = tokens[0];
 
-    switch (taskNumber) {
-      case 1 -> simpleMapping();
-      case 2 -> advancedMapping();
-      case 3 -> projection();
+    switch (command) {
+      case "find" -> printManagerToJson(Long.parseLong(tokens[1]));
+      default -> System.out.println("Invalid command")
     }
 
   }
 
-  private void projection() {
-    List<EmployeeDto> employees = this.employeeService.
-            findAllEmployeesBornBefore1990(LocalDate.of(1990, 1, 1));
+  private void printManagerToJson(long parseLong) {
 
-    if (employees.size() == 0) {
-      System.out.println("There are no employees born before 1990");
-    } else {
-      employees
-              .stream()
-              .map(e -> String.format("%s %s %.2f - Manager: %s",
-                      e.getFirstName(),
-                      e.getLastName(),
-                      e.getSalary(),
-                      e.getManager() != null ? e.getManager().getLastName() : "[no manager]"))
-              .forEach(System.out::println);
-    }
   }
 
-  private void advancedMapping() {
-    // Nested mapping
-    System.out.println("Please select manager id: ");
-    int managerId = Integer.parseInt(scanner.nextLine());
-
-    ManagerDto manager = this.employeeService.findManagerById(managerId);
-
-    System.out.printf("%s %s | Employees: %d\n%s\n",
-            manager.getFirstName(),
-            manager.getLastName(),
-            manager.getSubordinates().size(),
-            getSubordinatesData(manager.getSubordinates()));
-  }
-
-  private String getSubordinatesData(List<EmployeeDto> employees) {
-    return employees
-            .stream()
-            .map(e -> String.format("\t- %s %s %.2f",
-                    e.getFirstName(),
-                    e.getLastName(),
-                    e.getSalary()))
-            .collect(Collectors.joining("\n"));
-  }
-
-  private void simpleMapping() {
-    System.out.println("Please select employee id: ");
-    int employeeId = Integer.parseInt(scanner.nextLine());
-
-    EmployeeDto employeeDto = this.employeeService.findEmployeeById(employeeId);
-
-    System.out.printf("%s %s %.2f\n", employeeDto.getFirstName(),
-            employeeDto.getLastName(),
-            employeeDto.getSalary());
-  }
 }

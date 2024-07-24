c617c8bcc028ae1de7ed9323ba743a747720380e
wolfie <yavorhristozov@gmail.com>
2024-07-24 19:37:16 +0300

    add : @Gson bean

--- 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/ApplicationBeanConf.java
+++ 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/ApplicationBeanConf.java
@@ -1,5 +1,7 @@
 package com.example.mappingobjectslab.configuration;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +11,15 @@ import java.util.Scanner;
 public class ApplicationBeanConf {
 
   @Bean
-  public Scanner scanner(){
+  public Scanner scanner() {
     return new Scanner(System.in);
+  }
+
+  @Bean
+  public Gson gson() {
+    return new GsonBuilder()
+            .setPrettyPrinting()
+            .excludeFieldsWithoutExposeAnnotation()
+            .create();
   }
 }

fb52ccaf02bbe6ca6a41cf5bd8d7d0ad6569bfb4
wolfie <yavorhristozov@gmail.com>
2024-07-25 14:37:20 +0300

    add : ShoppingCartRepository

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/repository/ShoppingCartRepository.java
@@ -0,0 +1,9 @@
+package com.example.dto_exercise.repository;
+
+import com.example.dto_exercise.model.entity.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart,Long> {
+}

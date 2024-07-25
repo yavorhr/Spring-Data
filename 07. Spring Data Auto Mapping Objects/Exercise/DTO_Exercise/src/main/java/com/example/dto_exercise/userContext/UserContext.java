433f3fc1306e40d2b17101c560dd13311fbd429a
wolfie <yavorhristozov@gmail.com>
2024-07-25 13:43:21 +0300

    add : ValidationUtilImpl and UserContext

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/userContext/UserContext.java
@@ -0,0 +1,72 @@
+package com.example.dto_exercise.userContext;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserContext {
+  private Long id;
+  private String email;
+  private String fullName;
+  private String password;
+  private boolean isAdmin;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public boolean isAdmin() {
+    return isAdmin;
+  }
+
+  public void setAdmin(boolean admin) {
+    isAdmin = admin;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public boolean isUserLoggedIn() {
+    return this.id == 0;
+  }
+
+  public String getFullName() {
+    return fullName;
+  }
+
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
+
+  public void saveSession(Long id, String email, String fullName, String password, boolean isAdmin) {
+    this.id = id;
+    this.email = email;
+    this.fullName = fullName;
+    this.password = password;
+    this.isAdmin = isAdmin;
+  }
+
+  public void removeSession() {
+    this.id = 0L;
+    this.email = "";
+    this.fullName = "";
+    this.password = "";
+  }
+
+}

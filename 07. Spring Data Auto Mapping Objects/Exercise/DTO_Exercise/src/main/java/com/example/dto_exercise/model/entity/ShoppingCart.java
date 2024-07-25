b05d97199f8a151a9cab93c826d081a69ed95096
wolfie <yavorhristozov@gmail.com>
2024-07-25 14:32:32 +0300

    add : ShoppingCart entity

---
+++ 07. Spring Data Auto Mapping Objects/Exercise/DTO_Exercise/src/main/java/com/example/dto_exercise/model/entity/ShoppingCart.java
@@ -0,0 +1,35 @@
+package com.example.dto_exercise.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "shopping_cart")
+public class ShoppingCart extends BaseEntity {
+  private User user;
+  private Set<Game> addedGames;
+
+  public ShoppingCart() {
+    this.addedGames = new HashSet<>();
+  }
+
+  @OneToOne
+  public User getUser() {
+    return user;
+  }
+
+  @OneToMany
+  public Set<Game> getAddedGames() {
+    return addedGames;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+
+  public void setAddedGames(Set<Game> addedGames) {
+    this.addedGames = addedGames;
+  }
+}

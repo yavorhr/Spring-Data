4bbafe958ce9aed27786016fd733c37cf25e7965
wolfie <yavorhristozov@gmail.com>
2024-07-25 13:28:56 +0300

    add : findAll-to()
    persist to json file

--- 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
+++ 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
@@ -8,6 +8,10 @@ import com.google.gson.Gson;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -32,12 +36,32 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
       String command = tokens[0];
 
       switch (command) {
+        //find 1
         case "find" -> findById(Long.parseLong(tokens[1]));
         case "findAll" -> findAll();
+        //save {"firstName":"Ivan","lastName":"Ivanov","salary":3000.00,"birthday":"2024-07-04","address":"Chirpan","isOnHoliday":true}
         case "save" -> saveManager(tokens[1]);
+        //save-from-file src/main/resources/manager.json
+        case "save-from-file" -> saveManagerFromFilePath(tokens[1]);
+        //findAll-to src/main/resources/allManagers.json
+        case "findAll-to" -> saveAllManagersToFile(tokens[1]);
         default -> System.out.println("Invalid command");
       }
     }
+  }
+
+  private void saveAllManagersToFile(String path) throws IOException {
+    List<ManagerDto> allManagers = this.employeeService.findAll();
+    this.gson.toJson(allManagers, new FileWriter(path));
+  }
+
+  private void saveManagerFromFilePath(String path) throws FileNotFoundException {
+    RequestCreateManagerDto manager = this.gson.fromJson(new FileReader(path), RequestCreateManagerDto.class);
+    ResponseCreateManagerDto savedManager = this.employeeService.save(manager);
+
+    System.out.println(this.gson.toJson(savedManager));
+
+
   }
 
   private void saveManager(String json) {
--- 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
+++ 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/EmployeeDto.java
@@ -6,25 +6,11 @@ import java.math.BigDecimal;
 
 public class EmployeeDto extends BasicDto {
   @Expose
-  private String firstName;
-  @Expose
-  private String lastName;
-  @Expose
   private BigDecimal salary;
   @Expose
   private String manager;
 
   public EmployeeDto() {
-  }
-
-  @Override
-  public String getFirstName() {
-    return firstName;
-  }
-
-  @Override
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
   }
 
   public BigDecimal getSalary() {
@@ -43,15 +29,6 @@ public class EmployeeDto extends BasicDto {
     this.manager = manager.getFirstName();
   }
 
-  @Override
-  public String getLastName() {
-    return lastName;
-  }
-
-  @Override
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
 
 
 }

feb4cd4071172f68f86b1a287794d26699b40180
wolfie <yavorhristozov@gmail.com>
2024-07-25 12:31:21 +0300

    add : ModelMapper @Bean

--- 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/ApplicationBeanConf.java
+++ 08. JSON Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/ApplicationBeanConf.java
@@ -2,6 +2,7 @@ package com.example.mappingobjectslab.configuration;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,5 +22,10 @@ public class ApplicationBeanConf {
             .setPrettyPrinting()
             .excludeFieldsWithoutExposeAnnotation()
             .create();
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
   }
 }

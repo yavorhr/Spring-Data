d6d7cf77d1556ca2e5e566aac1f430aa40db8d16
wolfie <yavorhristozov@gmail.com>
2024-07-26 15:04:59 +0300

    add : CategorySeedDto

---
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/model/dto/CategorySeedDto.java
@@ -0,0 +1,22 @@
+package com.example.productshop_practice.model.dto;
+
+import com.google.gson.annotations.Expose;
+import jakarta.validation.constraints.Size;
+
+public class CategorySeedDto {
+
+  @Expose
+  private String name;
+
+  public CategorySeedDto() {
+  }
+
+  @Size(min = 3, max = 15)
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}

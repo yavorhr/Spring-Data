5bc9fdda438c3174e5f9163d5e016105f10b3174
wolfie <yavorhristozov@gmail.com>
2024-07-26 14:31:30 +0300

    add : Product entity

---
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/model/entity/Product.java
@@ -0,0 +1,64 @@
+package com.example.productshop_practice.model.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "products")
+public class Product extends BaseEntity {
+  private String name;
+  private BigDecimal price;
+  private User buyer;
+  private User seller;
+  private Set<Category> categories;
+
+  public Product() {
+  }
+
+  @Column
+  public String getName() {
+    return name;
+  }
+
+  @Column
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  @ManyToMany
+  public Set<Category> getCategories() {
+    return categories;
+  }
+
+  @ManyToOne
+  public User getBuyer() {
+    return buyer;
+  }
+
+  @ManyToOne
+  public User getSeller() {
+    return seller;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public void setBuyer(User buyer) {
+    this.buyer = buyer;
+  }
+
+  public void setSeller(User seller) {
+    this.seller = seller;
+  }
+
+  public void setCategories(Set<Category> categories) {
+    this.categories = categories;
+  }
+}

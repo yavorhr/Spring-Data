71b4bf41d68b5febf45a1f132359a8ac75e1c312
wolfie <yavorhristozov@gmail.com>
2024-07-26 14:20:53 +0300

    add : User entity

--- 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/model/entity/User.java
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/model/entity/User.java
@@ -1,11 +1,43 @@
 package com.example.productshop_practice.model.entity;
 
-import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
+  private int age;
+  private String firstName;
+  private String lastName;
 
+  public User() {
+  }
+
+  @Column
+  public int getAge() {
+    return age;
+  }
+
+  @Column(name = "first_name")
+  public String getFirstName() {
+    return firstName;
+  }
+
+  @Column(name = "last_name",nullable = false)
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 }

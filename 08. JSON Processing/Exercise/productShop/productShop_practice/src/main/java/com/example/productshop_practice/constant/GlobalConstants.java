05c7af14e862b6c4cf48a4f02ad22c5411a8ae15
wolfie <yavorhristozov@gmail.com>
2024-07-26 14:48:16 +0300

    add : Constants paths

--- 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/constant/GlobalConstants.java
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/constant/GlobalConstants.java
@@ -4,4 +4,7 @@ public class GlobalConstants {
   public static final String INPUT_RESOURCES_FILE_PATH = "src/main/resources/input_files/";
   public static final String OUTPUT_RESOURCES_FILE_PATH = "src/main/resources/output_files/";
 
+  public static final String CATEGORIES_INPUT_FILE_PATH = "categories.json";
+  public static final String USERS_INPUT_FILE_PATH = "users.json";
+  public static final String PRODUCTS_INPUT_FILE_PATH = "products.json";
 }

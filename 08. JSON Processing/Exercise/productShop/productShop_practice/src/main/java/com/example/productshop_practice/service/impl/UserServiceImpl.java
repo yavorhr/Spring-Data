d8a2c69e10a9eb8a5990da54d8fbb3b0ba327c4e
wolfie <yavorhristozov@gmail.com>
2024-07-26 15:25:24 +0300

    add : UserRepository

--- 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/service/impl/UserServiceImpl.java
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/service/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.productshop_practice.service.impl;
 
 import com.example.productshop_practice.service.UserService;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserServiceImpl implements UserService {
 }

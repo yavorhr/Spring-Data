5b93e860c98d29339d25853b0d9568f10027de23
wolfie <yavorhristozov@gmail.com>
2024-07-26 13:58:23 +0300

    init

---
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/ProductShopPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.productshop_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductShopPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProductShopPracticeApplication.class, args);
+  }
+
+}

3bc0fb87c3ca43da15ed8b574cdead0f482906ba
wolfie <yavorhristozov@gmail.com>
2024-07-26 14:43:09 +0300

    add : GlobalConstants

---
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/constant/GlobalConstants.java
@@ -0,0 +1,7 @@
+package com.example.productshop_practice.constant;
+
+public class GlobalConstants {
+  public static final String INPUT_RESOURCES_FILE_PATH = "src/main/resources/input_files/";
+  public static final String OUTPUT_RESOURCES_FILE_PATH = "src/main/resources/output_files/";
+
+}

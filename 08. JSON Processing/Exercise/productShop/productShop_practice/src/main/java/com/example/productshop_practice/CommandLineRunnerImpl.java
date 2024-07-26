da2e14458b189aff8d6bca3d3e55d5542717cb93
wolfie <yavorhristozov@gmail.com>
2024-07-26 14:50:38 +0300

    add : hash & equals

--- 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/CommandLineRunnerImpl.java
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/CommandLineRunnerImpl.java
@@ -1,13 +1,23 @@
 package com.example.productshop_practice;
 
+import com.example.productshop_practice.service.CategoriesService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
+  private final CategoriesService categoriesService;
+
+  public CommandLineRunnerImpl(CategoriesService categoriesService) {
+    this.categoriesService = categoriesService;
+  }
 
   @Override
   public void run(String... args) throws Exception {
+    seedData();
+  }
 
+  private void seedData() {
+    this.categoriesService.seedCategories();
   }
 }

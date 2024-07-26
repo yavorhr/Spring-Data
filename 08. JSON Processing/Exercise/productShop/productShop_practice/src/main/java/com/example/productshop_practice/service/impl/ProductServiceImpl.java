d4c0f890be929c6c1594aecef03047284a2de76b
wolfie <yavorhristozov@gmail.com>
2024-07-26 15:39:55 +0300

    add : ProductService

---
+++ 08. JSON Processing/Exercise/productShop/productShop_practice/src/main/java/com/example/productshop_practice/service/impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.productshop_practice.service.impl;
+
+import com.example.productshop_practice.service.ProductService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductServiceImpl implements ProductService {
+}

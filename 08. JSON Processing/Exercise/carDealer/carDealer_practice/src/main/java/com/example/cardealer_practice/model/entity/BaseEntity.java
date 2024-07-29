3fb3b6ad35fe4c5d808ce53a7da4ba604ab860a1
wolfie <yavorhristozov@gmail.com>
2024-07-29 19:07:04 +0300

    add : Supplier entity

--- 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/entity/BaseEntity.java
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/entity/BaseEntity.java
@@ -1,9 +1,14 @@
 package com.example.cardealer_practice.model.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   public BaseEntity() {

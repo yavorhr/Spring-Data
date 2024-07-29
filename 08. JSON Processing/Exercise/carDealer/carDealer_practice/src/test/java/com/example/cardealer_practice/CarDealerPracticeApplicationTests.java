f5c358f2e6d85d7a1641d7c498af24f0a2303bab
wolfie <yavorhristozov@gmail.com>
2024-07-29 18:50:49 +0300

    init : spring project

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/test/java/com/example/cardealer_practice/CarDealerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardealer_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealerPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

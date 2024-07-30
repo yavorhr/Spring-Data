be0fcc8b90550c1acc7c0a317e49f600e3673969
wolfie <yavorhristozov@gmail.com>
2024-07-30 16:21:06 +0300

    add @Bean for BufferedReader

--- 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/conf/ApplicationBeanConfiguration.java
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/conf/ApplicationBeanConfiguration.java
@@ -6,6 +6,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 @Configuration
 public class ApplicationBeanConfiguration {
 
@@ -20,5 +24,10 @@ public class ApplicationBeanConfiguration {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public BufferedReader bufferedReader() {
+    return new BufferedReader(new InputStreamReader(System.in));
   }
 }

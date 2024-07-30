5bf65ca710b09cbde7fb56a7167e8a3c63bf1584
wolfie <yavorhristozov@gmail.com>
2024-07-30 16:24:15 +0300

    add : SupplierService

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/service/impl/SupplerServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.cardealer_practice.model.service.impl;
+
+import com.example.cardealer_practice.model.service.SupplerService;
+
+public class SupplerServiceImpl implements SupplerService {
+}

2d6fb3183dab0ecd0a27db1a508f18a3e271109d
wolfie <yavorhristozov@gmail.com>
2024-07-30 15:49:21 +0300

    add : Supplier entity

--- 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/CarDealerPracticeApplication.java
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/CarDealerPracticeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDealerPracticeApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(CarDealerPracticeApplication.class, args);
   }
---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/entity/Supplier.java
@@ -0,0 +1,44 @@
+package com.example.cardealer_practice.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "suppliers")
+public class Supplier extends BaseEntity {
+    private String name;
+    private boolean isImporter;
+    private List<Part> parts;
+
+    public Supplier() {
+    }
+
+    @Column
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "is_importer")
+    public boolean isImporter() {
+        return isImporter;
+    }
+
+    public void setImporter(boolean importer) {
+        isImporter = importer;
+    }
+
+    @OneToMany(mappedBy = "supplier",fetch = FetchType.EAGER)
+    public List<Part> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Part> parts) {
+        this.parts = parts;
+    }
+}

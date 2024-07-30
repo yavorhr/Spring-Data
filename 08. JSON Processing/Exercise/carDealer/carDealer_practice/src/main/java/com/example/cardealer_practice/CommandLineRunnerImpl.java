81064da84499c5b909f20431a2262389e7f8be53
wolfie <yavorhristozov@gmail.com>
2024-07-30 16:06:05 +0300

    add : CLR
    i/o

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/CommandLineRunnerImpl.java
@@ -0,0 +1,14 @@
+package com.example.cardealer_practice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
+}

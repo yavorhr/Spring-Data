7af5381979758a30a62aaa8837833de94c704297
wolfie <yavorhristozov@gmail.com>
2024-07-30 18:50:15 +0300

    add : ValidationUtil
    Spring validator impl

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/util/ValidationUtil.java
@@ -0,0 +1,6 @@
+package com.example.cardealer_practice.model.util;
+
+public interface ValidationUtil {
+
+  <E> boolean isValid(E entity);
+}
---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/util/ValidationUtilImpl.java
@@ -0,0 +1,19 @@
+package com.example.cardealer_practice.model.util;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidationUtilImpl implements ValidationUtil {
+  private final Validator validator;
+
+  public ValidationUtilImpl() {
+    this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
+
+  @Override
+  public <E> boolean isValid(E entity) {
+    return validator.validate(entity).isEmpty();
+  }
+}

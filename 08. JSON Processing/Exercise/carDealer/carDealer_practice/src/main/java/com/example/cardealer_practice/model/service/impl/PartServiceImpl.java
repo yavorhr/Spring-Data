5ba01105a28844df359aed94832210e95b6ef83e
wolfie <yavorhristozov@gmail.com>
2024-07-30 18:58:38 +0300

    add : PartService

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/service/impl/PartServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cardealer_practice.model.service.impl;
+
+import com.example.cardealer_practice.model.service.PartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PartServiceImpl implements PartService {
+}

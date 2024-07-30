58ecd8e6eb89828ea9a1476548b312b41ca1e709
wolfie <yavorhristozov@gmail.com>
2024-07-30 16:30:42 +0300

    add : ProjectConstant file

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/constant/ProjectConstants.java
@@ -0,0 +1,20 @@
+package com.example.cardealer_practice.constant;
+
+public class ProjectConstants {
+  // Input
+  private static final String INPUT_FOLDER = "src/main/resources/input";
+
+  private static final String SUPPLIERS = "suppliers.json";
+  private static final String CARS = "cars.json";
+  private static final String CUSTOMERS = "customers.json";
+  private static final String PARTS = "parts.json";
+  private static final String SALES = "sales.json";
+
+  private static final String SAVE_SUPPLIERS_TO_FILE = INPUT_FOLDER + SUPPLIERS;
+  private static final String SAVE_CARS_TO_FILE = INPUT_FOLDER + CARS;
+  private static final String SAVE_CUSTOMERS_TO_FILE = INPUT_FOLDER + CUSTOMERS;
+  private static final String SAVE_PARTS_TO_FILE = INPUT_FOLDER + PARTS;
+  private static final String SAVE_SALES_TO_FILE = INPUT_FOLDER + SALES;
+
+
+}

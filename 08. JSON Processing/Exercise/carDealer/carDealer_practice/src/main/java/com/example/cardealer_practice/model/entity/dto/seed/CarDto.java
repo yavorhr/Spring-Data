8374461b591d74882af11e5a18b57bbf852e29f5
wolfie <yavorhristozov@gmail.com>
2024-07-30 19:14:19 +0300

    add : CarDto

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/entity/dto/seed/CarDto.java
@@ -0,0 +1,39 @@
+package com.example.cardealer_practice.model.entity.dto.seed;
+
+import com.google.gson.annotations.Expose;
+
+public class CarDto {
+  @Expose
+  private String make;
+  @Expose
+  private String model;
+  @Expose
+  private Long travelledDistance;
+
+  public CarDto() {
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public void setMake(String make) {
+    this.make = make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public Long getTravelledDistance() {
+    return travelledDistance;
+  }
+
+  public void setTravelledDistance(Long travelledDistance) {
+    this.travelledDistance = travelledDistance;
+  }
+}

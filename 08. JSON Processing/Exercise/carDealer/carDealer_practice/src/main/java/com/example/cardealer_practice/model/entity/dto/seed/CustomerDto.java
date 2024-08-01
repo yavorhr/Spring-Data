0246cc30bf89863a0ed3fa82d620ae46362f410e
wolfie <yavorhristozov@gmail.com>
2024-08-01 11:05:05 +0300

    add : CustomerDto

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/entity/dto/seed/CustomerDto.java
@@ -0,0 +1,44 @@
+package com.example.cardealer_practice.model.entity.dto.seed;
+
+import com.google.gson.annotations.Expose;
+import jakarta.validation.constraints.Size;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+public class CustomerDto {
+  @Expose
+  @Size(min = 2)
+  private String name;
+  @Expose
+  private LocalDate birthDate;
+  @Expose
+  private boolean isYoungDriver;
+
+  public CustomerDto() {
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDate getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(LocalDate birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public boolean isYoungDriver() {
+    return isYoungDriver;
+  }
+
+  public void setYoungDriver(boolean youngDriver) {
+    isYoungDriver = youngDriver;
+  }
+}

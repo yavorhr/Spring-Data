1904e9b6d1a85d0526d2d7af753a5120d2f1718b
wolfie <yavorhristozov@gmail.com>
2024-08-01 11:17:22 +0300

    add : SaleService and SalveService Impl

---
+++ 08. JSON Processing/Exercise/carDealer/carDealer_practice/src/main/java/com/example/cardealer_practice/model/service/impl/SaleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cardealer_practice.model.service.impl;
+
+import com.example.cardealer_practice.model.service.SaleService;
+import org.hibernate.annotations.SecondaryRow;
+
+@SecondaryRow
+public class SaleServiceImpl implements SaleService {
+}

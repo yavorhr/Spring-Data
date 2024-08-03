15fa492bb4b465b9d28f5f91aa50480600467c57
wolfie <yavorhristozov@gmail.com>
2024-08-03 19:42:33 +0300

    add : LocalDateTime adapter

---
+++ 09. XML Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/configuration/LocalDateTimeAdapter.java
@@ -0,0 +1,21 @@
+package com.example.mappingobjectslab.configuration;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
+
+    @Override
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(
+                v.trim(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        );
+    }
+
+    @Override
+    public String marshal(LocalDateTime v) throws Exception {
+        return v.format( DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+}

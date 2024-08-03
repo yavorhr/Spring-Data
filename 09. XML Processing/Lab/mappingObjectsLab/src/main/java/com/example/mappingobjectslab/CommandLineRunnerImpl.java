9470c7516f8fbc02c0b4023699a8a8df07870094
wolfie <yavorhristozov@gmail.com>
2024-08-03 19:44:19 +0300

    refact : Main

--- 09. XML Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
+++ 09. XML Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/CommandLineRunnerImpl.java
@@ -1,113 +1,160 @@
-package com.example.mappingobjectslab;
+package com.example.objectmapping;
 
-
-import com.example.mappingobjectslab.entity.dto.ManagerCollection;
-import com.example.mappingobjectslab.entity.dto.ManagerDto;;
-import com.example.mappingobjectslab.entity.dto.RequestCreateManagerDto;
-import com.example.mappingobjectslab.entity.dto.ResponseCreateManagerDto;
-import com.example.mappingobjectslab.services.EmployeeService;
-import jakarta.xml.bind.*;
+import com.example.objectmapping.models.dto.*;
+import com.example.objectmapping.models.dto.datetimetest.DateTimeSource;
+import com.example.objectmapping.models.dto.datetimetest.DateTimeTarget;
+import com.example.objectmapping.models.dto.firstlastnametest.FirstLastNameSource;
+import com.example.objectmapping.models.dto.firstlastnametest.FullNameTarget;
+import com.example.objectmapping.repositories.EmployeeRepository;
+import com.example.objectmapping.services.EmployeeService;
+import com.example.objectmapping.services.util.FormatConverter;
+import com.example.objectmapping.services.util.FormatConverterFactory;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-
-/*
- * 1. find X -> return JSON for Manager with subordinates
- * 2. findAll -> return JSON[] for all managers with their subordinates
- * 3. save JSON object -> save manager from JSON text to the database and return ResponseCreateDto
- * 4. save-from-file -> save Manager from json file from resources folder
- * 5. findAll-to -> fid all managers and write them into file
- *
- * -- insert manually employees in DB for testing
- * */
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
-public class CommandLineRunnerImpl implements CommandLineRunner {
-  private final Scanner scanner;
+public class Main implements CommandLineRunner {
+
   private final EmployeeService employeeService;
 
+  private final FormatConverterFactory factory;
 
-  public CommandLineRunnerImpl(Scanner scanner, EmployeeService employeeService) {
-    this.scanner = scanner;
+  private final EmployeeRepository employeeRepository;
+
+  private final ModelMapper modelMapper;
+
+  public Main(EmployeeService employeeService, FormatConverterFactory factory, EmployeeRepository employeeRepository, ModelMapper modelMapper) {
     this.employeeService = employeeService;
+    this.factory = factory;
+    this.employeeRepository = employeeRepository;
+    this.modelMapper = modelMapper;
   }
 
   @Override
   public void run(String... args) throws Exception {
 
+    Stream.of(1,2,3,4,6)
+            .map(el -> el * 2)
+            .peek(el -> System.out.println(el * 2))
+            .collect(Collectors.toList());
 
-    while (true) {
-      System.out.println("Please insert command: ");
-      String[] tokens = scanner.nextLine().split(" ");
-      String command = tokens[0];
 
-      switch (command) {
-        case "find" -> findById(Long.parseLong(tokens[1]));
-        case "findAll" -> findAll();
-        case "save" -> saveManager(tokens[1]);
-        case "save-from-file" -> saveManagerFromFilePath(tokens[1]);
-        case "findAll-to" -> saveAllManagersToFile(tokens[1]);
-        default -> System.out.println("Invalid command");
+
+//        var source = new DateTimeSource();
+//        source.setDate("2020-11-11 12:34:56");
+//
+//        DateTimeTarget target = this.modelMapper.map(
+//                source,
+//                DateTimeTarget.class
+//        );
+
+
+    var source = new FirstLastNameSource();
+    source.setFirstName("pesho");
+    source.setLastName("petrov");
+
+    var target = this.modelMapper.map(
+            source,
+            FullNameTarget.class
+    );
+
+    List<EmployeeAverageSalaryDto> allWithAvgSalary = this.employeeRepository.findAllWithAvgSalary();
+//        "2020-05-05 16:40:50"
+    var sc = new Scanner(System.in);
+
+    System.out.println("Enter format type: ");
+    String formatType = sc.nextLine();
+
+    FormatConverter converter = this.factory.create(formatType);
+    converter.setPrettyPrint();
+
+    TestDto testDto = converter.deserializeFromFile(
+            "./src/main/resources/test-za-dati.xml",
+            TestDto.class
+    );
+
+    var line = sc.nextLine();
+
+
+
+    while (!line.equals("end")) {
+      var cmdParts = line.split(" ", 2);
+      switch (cmdParts[0]) {
+        case "find":
+          Long id = Long.parseLong(cmdParts[1]);
+          ManagerDto managerById = this.employeeService.findOne(id);
+
+          System.out.println(converter.serialize(managerById));
+          break;
+        case "findAll":
+          List<ManagerDto> allManagers = this.employeeService.findAll();
+          System.out.println(converter.serialize(new ManagerCollection(allManagers)));
+          break;
+        case "save":
+          String input = cmdParts[1];
+
+          EmployeeCreateRequest request
+                  = converter.deserialize(input, EmployeeCreateRequest.class);
+
+
+          EmployeeCreateResponse response = this.employeeService.save(
+                  request
+          );
+
+          System.out.println(converter.serialize(response));
+          break;
+        case "save-from-file":
+          EmployeeCreateRequest fileRequest = converter.deserializeFromFile(
+                  cmdParts[1],
+                  EmployeeCreateRequest.class
+          );
+
+          EmployeeCreateResponse fileResponse = this.employeeService.save(
+                  fileRequest
+          );
+
+          System.out.println(converter.serialize(fileResponse));
+          break;
+        case "findAll-to":
+          List<ManagerDto> managers = this.employeeService.findAll();
+          converter.serialize(
+                  new ManagerCollection(managers),
+                  cmdParts[1] + "." + formatType
+          );
+
+          System.out.println("Written to file " + cmdParts[1]);
+
+          break;
+        case "change-format":
+          converter = this.factory.create(cmdParts[1]);
+          System.out.println("Format changed to " + cmdParts[1]);
+          break;
+        case "pretty-print":
+          converter.setPrettyPrint();
+          System.out.println("Success");
+          break;
       }
+
+      line = sc.nextLine();
     }
+
+
+//
+//        List<ManagerDto> managers = this.employeeService.findAll();
+//        managers.forEach(managerDto -> {
+//            if (managerDto.getSubordinates().isEmpty()) {
+//                return;
+//            }
+//            System.out.println(managerDto.getFirstName() + " " + managerDto.getLastName() + " (" + managerDto.getSubordinates().size() + "):");
+//            managerDto.getSubordinates().forEach(employeeDto -> {
+//                System.out.println("\t" + employeeDto.getFirstName() + " " + employeeDto.getLastName() + " : " + employeeDto.getIncome());
+//            });
+//        });
   }
-
-  private void saveAllManagersToFile(String path) throws IOException {
-    List<ManagerDto> allManagers = this.employeeService.findAll();
-
-  }
-
-  private void saveManagerFromFilePath(String path) throws FileNotFoundException {
-////    RequestCreateManagerDto manager = this.gson.fromJson(new FileReader(path), RequestCreateManagerDto.class);
-//    ResponseCreateManagerDto savedManager = this.employeeService.save(manager);
-
-//    System.out.println(this.gson.toJson(savedManager));
-  }
-
-  private void saveManager(String input) throws JAXBException {
-
-    /*
-* <employee first_name="..." last_name="..." >
-    <salary>2000</salary>
-    <address>Mladost</address>
-* </employee>
-*
-** <employee>first_name="XML" last_name="XML"><salary>2000</salary><address>Mladost</address></employee>
-* */
-
-    JAXBContext jaxbContext = JAXBContext.newInstance(ManagerCollection.class);
-    Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-    unmarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-    RequestCreateManagerDto request = (RequestCreateManagerDto)
-            unmarshaller.unmarshal(new ByteArrayInputStream(input.getBytes()));
-
-    this.employeeService.save(request);
-
-  }
-
-  private void findAll() throws JAXBException {
-    JAXBContext jaxbContext = JAXBContext.newInstance(ManagerCollection.class);
-    Marshaller managerCollection = jaxbContext.createMarshaller();
-    managerCollection.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-    List<ManagerDto> managersList = this.employeeService.findAll();
-
-    managerCollection.marshal(new ManagerCollection(managersList), System.out);
-  }
-
-  private void findById(long managerId) throws JAXBException, JAXBException {
-    JAXBContext jaxbContext = JAXBContext.newInstance(ManagerDto.class);
-    Marshaller managerMarshaller = jaxbContext.createMarshaller();
-    managerMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-    ManagerDto manager = this.employeeService.findManagerById(managerId);
-    managerMarshaller.marshal(manager, System.out);
-  }
-
 }

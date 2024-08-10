0ac30f87c96bce55b4303defccffc5e157a25cd1
wolfie <yavorhristozov@gmail.com>
2024-08-10 13:00:57 +0300

    add : DateTimeTarget

---
+++ 09. XML Processing/Lab/mappingObjectsLab/src/main/java/com/example/mappingobjectslab/entity/dto/datetimesheet/DateTimeTarget.java
@@ -0,0 +1,16 @@
+package com.example.mappingobjectslab.entity.dto.datetimesheet;
+
+import java.time.LocalDateTime;
+
+public class DateTimeTarget {
+
+    private LocalDateTime date;
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}

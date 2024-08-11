dc6bbd7850d033b597acbe353a7d0f39a54078e5
wolfie <yavorhristozov@gmail.com>
2024-08-11 13:30:09 +0300

    add : @Bean for BufferedReader

--- 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/conf/ApplicationBeanConfiguration.java
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/conf/ApplicationBeanConfiguration.java
@@ -1,14 +1,22 @@
 package com.example.shop.conf;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @Configuration
 public class ApplicationBeanConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Bean
+  public BufferedReader bufferedReader() {
+    return new BufferedReader(new InputStreamReader(System.in));
+  }
 
 }

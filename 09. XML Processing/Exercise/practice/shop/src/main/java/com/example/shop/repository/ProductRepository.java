b34d31d20d6e46616779a836f0a6c5b7b2cb1c15
wolfie <yavorhristozov@gmail.com>
2024-08-11 12:59:05 +0300

    add : ProductRepository
    JPA repositoriy

---
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.shop.repository;
+
+import com.example.shop.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+}

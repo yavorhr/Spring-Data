656dd172cacabc48995ed7b18b9a1123b0ce26e2
wolfie <yavorhristozov@gmail.com>
2024-08-11 13:03:40 +0300

    add : UserRepository

---
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.shop.repository;
+
+import com.example.shop.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}

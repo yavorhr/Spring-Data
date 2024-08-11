9923f406e6df7628b34874c62199b22521423ec0
wolfie <yavorhristozov@gmail.com>
2024-08-11 12:42:27 +0300

    add : CLR class and init structure

---
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/CommandLineRunnerImpl.java
@@ -0,0 +1,13 @@
+package com.example.shop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommandLineRunnerImpl implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
+}

619e36bd758467ddd187bda809e12d53409baedf
wolfie <yavorhristozov@gmail.com>
2024-08-11 13:06:04 +0300

    add : impl of Servieces

---
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shop.service.impl;
+
+import com.example.shop.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+}
---
+++ 09. XML Processing/Exercise/practice/shop/src/main/java/com/example/shop/service/impl/ProductServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.shop.service.impl;
+
+import com.example.shop.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+}

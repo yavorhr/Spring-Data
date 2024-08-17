1fc67987b6e388475b7dde30d587b4cc26388bdf
wolfie <yavorhristozov@gmail.com>
2024-08-17 12:41:41 +0300

    refact : ProjectConstants

--- 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/constant/ProjectConstants.java
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/constant/ProjectConstants.java
@@ -4,24 +4,30 @@ public class ProjectConstants {
   // INPUT
   public static final String INPUT_RESOURCES_FILES_PATH = "src/main/resources/input/";
 
-  public static final String CATEGORIES_FILE_NAME = "categories.xml";
-  public static final String USERS_FILE_NAME = "users.xml";
-  public static final String PRODUCTS_FILE_NAME = "products.xml";
+  public static final String CARS_FILE_NAME = "cars.xml";
+  public static final String CUSTOMERS_FILE_NAME = "customers.xml";
+  public static final String PARTS_FILE_NAME = "parts.xml";
+  public static final String SUPPLIERS_FILE_NAME = "suppliers.xml";
 
-  public static final String SEED_CATEGORIES = INPUT_RESOURCES_FILES_PATH + CATEGORIES_FILE_NAME;
-  public static final String SEED_USERS = INPUT_RESOURCES_FILES_PATH + USERS_FILE_NAME;
-  public static final String SEED_PRODUCTS = INPUT_RESOURCES_FILES_PATH + PRODUCTS_FILE_NAME;
+  public static final String SEED_CARS = INPUT_RESOURCES_FILES_PATH + CARS_FILE_NAME;
+  public static final String SEED_CUSTOMERS = INPUT_RESOURCES_FILES_PATH + CUSTOMERS_FILE_NAME;
+  public static final String SEED_PARTS = INPUT_RESOURCES_FILES_PATH + PARTS_FILE_NAME;
+  public static final String SEED_SUPPLIERS = INPUT_RESOURCES_FILES_PATH + SUPPLIERS_FILE_NAME;
 
   // OUTPUT
   public static final String OUTPUT_RESOURCE_FILES_PATH = "src/main/resources/output/";
 
-  public static final String PRODUCTS_IN_RANGE_FILE_NAME = "products-in-range.xml";
-  public static final String SOLD_PRODUCTS_FILE_NAME = "sold-products.xml";
-  public static final String EXTRACT_CATEGORIES = "extract-categories.xml";
-  public static final String FIND_USERS_WITH_MORE_THAN_ONE_PRODUCT_SOLD = "find-users-sold-products.xml";
+  public static final String ORDERED_CUSTOMERS_FILE_NAME = "ordered-customers.xml";
+  public static final String CARS_FROM_TOYOTA_FILE_NAME = "cars-from-make-toyota.xml";
+  public static final String LOCAL_SUPPLIERS_FILE_NAME = "local-suppliers.xml";
+  public static final String CARS_WITH_PARTS_FILE_NAME = "cars-with-their-list-of-parts.xml";
+  public static final String TOYOTA_SALES_FILE_NAME = "total-sales-by-customer.xml";
+  public static final String SALES_WITH_DISCOUNT_FILE_NAME = "sales-with-applied-discount.xml";
 
-  public static final String FIRST_QUERY = OUTPUT_RESOURCE_FILES_PATH + PRODUCTS_IN_RANGE_FILE_NAME;
-  public static final String SECOND_QUERY = OUTPUT_RESOURCE_FILES_PATH + SOLD_PRODUCTS_FILE_NAME;
-  public static final String THIRD_QUERY = OUTPUT_RESOURCE_FILES_PATH +  EXTRACT_CATEGORIES;
-  public static final String FOURTH_QUERY = OUTPUT_RESOURCE_FILES_PATH +FIND_USERS_WITH_MORE_THAN_ONE_PRODUCT_SOLD;
+  public static final String FIRST_QUERY = OUTPUT_RESOURCE_FILES_PATH + ORDERED_CUSTOMERS_FILE_NAME;
+  public static final String SECOND_QUERY = OUTPUT_RESOURCE_FILES_PATH + CARS_FROM_TOYOTA_FILE_NAME;
+  public static final String THIRD_QUERY = OUTPUT_RESOURCE_FILES_PATH +  LOCAL_SUPPLIERS_FILE_NAME;
+  public static final String FOURTH_QUERY = OUTPUT_RESOURCE_FILES_PATH +CARS_WITH_PARTS_FILE_NAME;
+  public static final String FIFTH_QUERY = OUTPUT_RESOURCE_FILES_PATH +TOYOTA_SALES_FILE_NAME;
+  public static final String SIXTH_QUERY = OUTPUT_RESOURCE_FILES_PATH +SALES_WITH_DISCOUNT_FILE_NAME;
 }

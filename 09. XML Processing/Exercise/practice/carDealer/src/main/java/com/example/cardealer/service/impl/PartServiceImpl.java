119d7e832b8c8cb235448fdd719e59030b0e9894
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:29:32 +0300

    add : PartServiceImpl

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/service/impl/PartServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cardealer.service.impl;
+
+import com.example.cardealer.service.PartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PartServiceImpl implements PartService {
+}

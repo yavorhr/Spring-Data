08f450992beecda3996e760d04a343a29f46e455
wolfie <yavorhristozov@gmail.com>
2024-08-17 12:20:25 +0300

    add : xmlParser impl

--- 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/CarDealerApplication.java
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/CarDealerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDealerApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(CarDealerApplication.class, args);
   }
-
 }
---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/util/impl/XmlParserImpl.java
@@ -0,0 +1,34 @@
+package com.example.cardealer.util.impl;
+
+import com.example.cardealer.util.XmlParser;
+import org.springframework.stereotype.Component;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+@Component
+public class XmlParserImpl implements XmlParser {
+    private JAXBContext jaxbContext;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T fromFile(String filePath, Class<T> dtoClass) throws JAXBException, FileNotFoundException {
+        jaxbContext = JAXBContext.newInstance(dtoClass);
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+        return (T) unmarshaller.unmarshal(new FileReader(filePath));
+    }
+
+    @Override
+    public <T> void writeToFile(String filePath, T dto) throws JAXBException {
+        jaxbContext = JAXBContext.newInstance(dto.getClass());
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        marshaller.marshal(dto,new File(filePath));
+    }
+}

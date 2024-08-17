585f20db5d19045230d861128b411412e78e43c1
wolfie <yavorhristozov@gmail.com>
2024-08-17 15:51:22 +0300

    add : CustomersRootViewDto

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/model/dto/view/FirstQuery/CustomersRootViewDto.java
@@ -0,0 +1,19 @@
+package com.example.cardealer.model.dto.view.FirstQuery;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "customers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomersRootViewDto {
+
+  @XmlElement(name = "customer")
+  private List<CustomerIdNameBdayIsYoungDriverDto> customers;
+
+  public void setCustomers(List<CustomerIdNameBdayIsYoungDriverDto> customers) {
+    this.customers = customers;
+  }
+}

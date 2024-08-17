cad640f67e418f8d9664cf257a5ba2151c58205e
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:03:52 +0300

    add : Car entity

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/model/entity/Car.java
@@ -0,0 +1,60 @@
+package com.example.cardealer.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+public class Car extends BaseEntity{
+
+    @Column(name = "make")
+    private String make;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "travelled_distance")
+    private long travelledDistance;
+
+    @ManyToMany
+    List<Part> parts;
+
+    public Car() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public long getTravelledDistance() {
+        return travelledDistance;
+    }
+
+    public void setTravelledDistance(long travelledDistance) {
+        this.travelledDistance = travelledDistance;
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<Part> parts) {
+        this.parts = parts;
+    }
+
+
+}

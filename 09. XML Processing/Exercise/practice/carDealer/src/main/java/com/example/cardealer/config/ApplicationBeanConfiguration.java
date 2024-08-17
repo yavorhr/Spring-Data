84edb76853380fd0d28e793c37146178f5234427
wolfie <yavorhristozov@gmail.com>
2024-08-17 15:12:00 +0300

    add : ModelMapper converter
    Convert from String to LocalDateTime

--- 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/config/ApplicationBeanConfiguration.java
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/config/ApplicationBeanConfiguration.java
@@ -1,23 +1,38 @@
 package com.example.cardealer.config;
 
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationBeanConfiguration {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+
+    Converter<String, LocalDateTime> stringToLocalDateTimeConverter = new Converter<String, LocalDateTime>() {
+      @Override
+      public LocalDateTime convert(MappingContext<String, LocalDateTime> context) {
+        return LocalDateTime.parse(context.getSource(), formatter);
+      }
+    };
+
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.addConverter(stringToLocalDateTimeConverter);
+
+    return modelMapper;
   }
 
   @Bean
   public BufferedReader bufferedReader() {
     return new BufferedReader(new InputStreamReader(System.in));
   }
-
 }

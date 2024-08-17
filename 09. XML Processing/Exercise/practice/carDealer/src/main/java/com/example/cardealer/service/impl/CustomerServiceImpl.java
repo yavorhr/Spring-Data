6b25f1fd63dfe9281a3f88cd14cebf77d4ac54e6
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:28:50 +0300

    add :  CustomerServiceImpl

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cardealer.service.impl;
+
+import com.example.cardealer.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+}

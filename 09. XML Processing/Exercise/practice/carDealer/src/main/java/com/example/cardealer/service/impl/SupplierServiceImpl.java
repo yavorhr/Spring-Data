3d1958b1a14e70f284be9cb53bb8c37b3cffa190
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:44:14 +0300

    cfg : add JAXB impl dep

--- 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/service/impl/SupplierServiceImpl.java
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/service/impl/SupplierServiceImpl.java
@@ -1,12 +1,16 @@
 package com.example.cardealer.service.impl;
 
 import com.example.cardealer.constant.ProjectConstants;
+import com.example.cardealer.model.dto.seed.SupplierRootDto;
 import com.example.cardealer.repository.SupplierRepository;
 import com.example.cardealer.service.SupplierService;
 import com.example.cardealer.util.ValidationUtil;
 import com.example.cardealer.util.XmlParser;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileNotFoundException;
 
 @Service
 public class SupplierServiceImpl implements SupplierService {
@@ -23,7 +27,14 @@ public class SupplierServiceImpl implements SupplierService {
   }
 
   @Override
-  public void seedSuppliers() {
+  public void seedSuppliers() throws JAXBException, FileNotFoundException {
+    if (this.supplierRepository.count() > 0) {
+      return;
+    }
 
+    SupplierRootDto rootDto =
+            this.xmlParser.fromFile(ProjectConstants.SEED_SUPPLIERS, SupplierRootDto.class);
+
+    System.out.println();
   }
 }

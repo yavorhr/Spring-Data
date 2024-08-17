61414ccee186c86b49970eb900fd7118066a1cb8
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:50:05 +0300

    add : CLIR

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/CommandLineRunnerImpl.java
@@ -0,0 +1,41 @@
+package com.example.cardealer;
+
+import com.example.cardealer.service.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+@Component
+public class CommandLineRunnerImpl implements CommandLineRunner {
+  private final BufferedReader bufferedReader;
+  private final CarService carService;
+  private final CustomerService customerService;
+  private final PartService partService;
+  private final SaleService saleService;
+  private final SupplierService supplierService;
+
+  public CommandLineRunnerImpl(BufferedReader bufferedReader, CarService carService, CustomerService customerService, PartService partService, SaleService saleService, SupplierService supplierService) {
+    this.bufferedReader = bufferedReader;
+    this.carService = carService;
+    this.customerService = customerService;
+    this.partService = partService;
+    this.saleService = saleService;
+    this.supplierService = supplierService;
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    seedData();
+
+//    System.out.println("Please select task number");
+//    int taskNumber = Integer.parseInt(this.bufferedReader.readLine());
+
+  }
+
+  private void seedData() throws JAXBException, FileNotFoundException {
+    this.supplierService.seedSuppliers();
+  }
+}

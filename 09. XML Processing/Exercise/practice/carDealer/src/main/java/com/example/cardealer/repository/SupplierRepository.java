7bc767cb7f0e5de65dc4b463d17fa3fea3af11cd
wolfie <yavorhristozov@gmail.com>
2024-08-17 13:01:09 +0300

    add : SupplierRepository
    JPA Repo

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/repository/SupplierRepository.java
@@ -0,0 +1,8 @@
+package com.example.cardealer.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+
+

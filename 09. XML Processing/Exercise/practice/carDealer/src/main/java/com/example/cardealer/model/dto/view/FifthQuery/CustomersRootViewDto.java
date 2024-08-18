cd289988dcb68f6703b45150bd4f4abfefe6053a
wolfie <yavorhristozov@gmail.com>
2024-08-18 19:41:44 +0300

    add : Dto for 5. Query

---
+++ 09. XML Processing/Exercise/practice/carDealer/src/main/java/com/example/cardealer/model/dto/view/FifthQuery/CustomersRootViewDto.java
@@ -0,0 +1,19 @@
+package com.example.cardealer.model.dto.view.FifthQuery;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+@XmlRootElement(name = "customers")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CustomersRootViewDto {
+
+  @XmlElement(name = "customer")
+  private List<CustomerFullNameCountCarsMoneySpentViewDto> customers;
+
+  public void setCustomers(List<CustomerFullNameCountCarsMoneySpentViewDto> customers) {
+    this.customers = customers;
+  }
+}

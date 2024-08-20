0b0db99f6f0712e0a5fb67743e5ce7a59e2a8836
wolfie <yavorhristozov@gmail.com>
2024-08-20 18:15:53 +0300

    init : project

---
+++ 13. Exam preparation/realDeal/src/test/java/com/example/realdeal/RealDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealDealApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

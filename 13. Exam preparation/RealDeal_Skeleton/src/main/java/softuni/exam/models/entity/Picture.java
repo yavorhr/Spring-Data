a20c1d023142b0a4feedbce784c8557c864a6a44
wolfie <yavorhristozov@gmail.com>
2024-08-20 13:43:39 +0300

    add : Picture entity

--- 13. Exam preparation/RealDeal_Skeleton/src/main/java/softuni/exam/models/entity/Picture.java
+++ 13. Exam preparation/RealDeal_Skeleton/src/main/java/softuni/exam/models/entity/Picture.java
@@ -2,6 +2,7 @@ package softuni.exam.models.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
@@ -11,8 +12,14 @@ import java.time.LocalDateTime;
 public class Picture extends BaseEntity {
   private String name;
   private LocalDateTime dateAndTime;
+  private Car car;
 
   public Picture() {
+  }
+
+  @ManyToOne
+  public Car getCar() {
+    return car;
   }
 
   @Column(length = 19, unique = true)
@@ -31,5 +38,9 @@ public class Picture extends BaseEntity {
 
   public void setDateAndTime(LocalDateTime dateAndTime) {
     this.dateAndTime = dateAndTime;
+  }
+
+  public void setCar(Car car) {
+    this.car = car;
   }
 }

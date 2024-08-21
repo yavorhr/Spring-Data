fc969629153bb198498164ec6175f9f94f260fb7
wolfie <yavorhristozov@gmail.com>
2024-08-21 13:09:59 +0300

    add : BaseEntity class
    inherit Long id

---
+++ 13. Exam preparation/realDeal/src/main/java/com/example/realdeal/model/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.example.realdeal.model.entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public BaseEntity() {
+    }
+}

fe93e63f37b476ff93ddc91e7f239f2b1f727ef8
wolfie <yavorhristozov@gmail.com>
2024-08-21 16:32:17 +0300

    add : Constellation entity

---
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 29 July 2023/Find-a-Star Skeleton/src/main/java/softuni/exam/models/entity/Constellation.java
@@ -0,0 +1,45 @@
+package softuni.exam.models.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "constellations")
+public class Constellation extends BaseEntity {
+  
+  private String name;
+  private String description;
+  private Set<Star> stars;
+
+  public Constellation() {
+  }
+
+  @Column(length = 20, unique = true)
+  public String getName() {
+    return name;
+  }
+
+  @Column(columnDefinition = "TEXT")
+  public String getDescription() {
+    return description;
+  }
+
+  @OneToMany
+  public Set<Star> getStars() {
+    return stars;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setStars(Set<Star> stars) {
+    this.stars = stars;
+  }
+}

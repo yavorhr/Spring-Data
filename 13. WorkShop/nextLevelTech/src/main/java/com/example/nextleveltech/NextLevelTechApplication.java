7b70a6005d3e5f7f2b4b81bd3b05b02f96f1ce0e
wolfie <yavorhristozov@gmail.com>
2024-08-23 14:41:26 +0300

    init : spring project

---
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/NextLevelTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextleveltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextLevelTechApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NextLevelTechApplication.class, args);
+  }
+
+}

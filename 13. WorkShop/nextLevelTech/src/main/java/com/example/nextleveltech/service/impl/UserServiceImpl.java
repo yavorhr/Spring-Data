c69cfaa1514b34931e8879d8ed22f664d7f98bdb
wolfie <yavorhristozov@gmail.com>
2024-08-23 18:36:28 +0300

    update UserServiceImpl
    impl   validateUserLoginDetails(UserLoginDto dto);

--- 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/service/impl/UserServiceImpl.java
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.example.nextleveltech.service.impl;
 
+import com.example.nextleveltech.model.dto.UserLoginDto;
 import com.example.nextleveltech.model.dto.UserRegisterDto;
 import com.example.nextleveltech.model.entity.User;
 import com.example.nextleveltech.repositoriy.UserRepository;
 import com.example.nextleveltech.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,15 +23,7 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public boolean registerUser(UserRegisterDto dto) {
-    if (this.userRepository.findByUsername(dto.getUsername()).isPresent()) {
-      return false;
-    }
-
-    if (this.userRepository.findByEmail(dto.getEmail()).isPresent()) {
-      return false;
-    }
-
-    if (!dto.getPassword().equals(dto.getConfirmPassword())) {
+    if (emailAlreadyTaken(dto) || passwordDoesNotMatch(dto) || usernameAlreadyTaken(dto)) {
       return false;
     }
 
@@ -36,5 +31,40 @@ public class UserServiceImpl implements UserService {
     this.userRepository.save(user);
 
     return true;
+  }
+
+  // Helpers
+  private boolean usernameAlreadyTaken(UserRegisterDto dto) {
+    if (this.userRepository.findByUsername(dto.getUsername()).isPresent()) {
+      return true;
+    }
+    return false;
+  }
+
+
+  private boolean emailAlreadyTaken(UserRegisterDto dto) {
+    if (this.userRepository.findByEmail(dto.getEmail()).isPresent()) {
+      return true;
+    }
+    return false;
+  }
+
+  private boolean passwordDoesNotMatch(UserRegisterDto dto) {
+    if (!dto.getPassword().equals(dto.getConfirmPassword())) {
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public Long validateUserLoginDetails(UserLoginDto dto) {
+    Optional<User> user =
+            this.userRepository.findByUsernameAndPassword(dto.getUsername(), dto.getPassword());
+
+    if (user.isEmpty()) {
+      return null;
+    }
+
+    return user.get().getId();
   }
 }

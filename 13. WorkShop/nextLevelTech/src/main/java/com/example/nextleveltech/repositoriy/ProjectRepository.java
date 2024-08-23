81e1403c454aee4061290db0b302b2f0a01bf262
wolfie <yavorhristozov@gmail.com>
2024-08-23 16:08:40 +0300

    add : Repositories

---
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/repositoriy/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.example.nextleveltech.repositoriy;
+
+import com.example.nextleveltech.model.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project,Long> {
+}

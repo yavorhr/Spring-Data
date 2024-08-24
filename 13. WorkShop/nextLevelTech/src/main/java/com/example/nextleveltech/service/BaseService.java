f8428d4037b556fd07efdd1734ce2c178d4367bf
wolfie <yavorhristozov@gmail.com>
2024-08-24 14:21:41 +0300

    update : BaseService
    add getXmlDataForImport()

--- 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/service/BaseService.java
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/service/BaseService.java
@@ -1,7 +1,11 @@
 package com.example.nextleveltech.service;
 
+import java.io.IOException;
+
 public interface BaseService {
 
   boolean areFilesImported();
+
+  String getXmlDataForImport() throws IOException;
 
 }

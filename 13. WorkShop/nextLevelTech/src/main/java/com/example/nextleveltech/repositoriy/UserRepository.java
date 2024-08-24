a9632df960a6c83d0ea5254ad76e1ed34f5f1fda
wolfie <yavorhristozov@gmail.com>
2024-08-24 13:27:40 +0300

    update : Repositories
    add existsAllBy();

--- 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/repositoriy/UserRepository.java
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/repositoriy/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   Optional<User> findByUsernameAndPassword(String username, String password);
+
+  boolean existsAllBy();
 }

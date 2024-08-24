f2ef27fa5705b0c121ccca5183fa80c2dcd27f95
wolfie <yavorhristozov@gmail.com>
2024-08-24 13:26:42 +0300

    update : Home controller
    add logic if user is logged in or not - if not logged in, redirect to "/", otherwise redirect to "/home"

--- 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/web/controllers/HomeController.java
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/web/controllers/HomeController.java
@@ -1,18 +1,25 @@
 package com.example.nextleveltech.web.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController extends BaseController {
 
   @GetMapping("/")
-  public String index(){
+  public String index(HttpServletRequest request){
+    if (this.isLogged(request)) {
+      return "redirect:/home";
+    }
     return "index";
   }
 
   @GetMapping("/home")
-  public String home(){
+  public String home(HttpServletRequest request){
+    if (!this.isLogged(request)) {
+      return "redirect:/";
+    }
     return "home";
   }
 

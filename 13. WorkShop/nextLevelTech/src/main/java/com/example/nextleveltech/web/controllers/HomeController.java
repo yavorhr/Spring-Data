88fe2394baad22b48efb0221316b7254233103ef
wolfie <yavorhristozov@gmail.com>
2024-08-24 13:38:19 +0300

    update : HomeController
    add Model with attribute "areImported"

--- 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/web/controllers/HomeController.java
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/web/controllers/HomeController.java
@@ -1,11 +1,24 @@
 package com.example.nextleveltech.web.controllers;
 
+import com.example.nextleveltech.service.CompanyService;
+import com.example.nextleveltech.service.EmployeeService;
+import com.example.nextleveltech.service.ProjectService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController extends BaseController {
+  private final CompanyService companyService;
+  private final EmployeeService employeeService;
+  private final ProjectService projectService;
+
+  public HomeController(CompanyService companyService, EmployeeService employeeService, ProjectService projectService) {
+    this.companyService = companyService;
+    this.employeeService = employeeService;
+    this.projectService = projectService;
+  }
 
   @GetMapping("/")
   public String index(HttpServletRequest request){
@@ -16,11 +29,18 @@ public class HomeController extends BaseController {
   }
 
   @GetMapping("/home")
-  public String home(HttpServletRequest request){
+  public String home(HttpServletRequest request, Model model){
     if (!this.isLogged(request)) {
       return "redirect:/";
     }
+
+    model.addAttribute("areImported", areImported());
     return "home";
   }
 
+  private boolean areImported() {
+    return this.companyService.areFilesImported()
+            && this.employeeService.areFilesImported()
+            && this.projectService.areFilesImported();
+  }
 }

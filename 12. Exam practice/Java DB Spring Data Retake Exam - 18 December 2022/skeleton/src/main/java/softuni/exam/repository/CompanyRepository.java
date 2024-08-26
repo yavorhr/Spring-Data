c6ae36b59516f94831df40f632eaf6c06f2922c2
wolfie <yavorhristozov@gmail.com>
2024-08-26 13:23:08 +0300

    add : CompanyRepository

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/CompanyRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/CompanyRepository.java
@@ -1,6 +1,10 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Company;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Long> {
 
 }

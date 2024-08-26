d6b66929f76551860c6b98a3a4b6275480152af4
wolfie <yavorhristozov@gmail.com>
2024-08-26 15:54:59 +0300

    update : ApplicationBeanConfiguration
    add addConverter for String to LocalDate

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -2,15 +2,30 @@ package softuni.exam.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class ApplicationBeanConfiguration {
 
   @Bean
   public ModelMapper modelMapper() {
+    ModelMapper modelMapper = new ModelMapper();
+
+    modelMapper.addConverter(new Converter<String, LocalDate>() {
+      @Override
+      public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+        return LocalDate
+                .parse(mappingContext.getSource(),
+                        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      }
+    });
     return new ModelMapper();
   }
 

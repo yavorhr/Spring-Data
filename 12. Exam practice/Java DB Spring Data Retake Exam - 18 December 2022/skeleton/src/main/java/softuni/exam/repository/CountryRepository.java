c0dd289161c8a621fca150d9f5191e5a29fdec11
wolfie <yavorhristozov@gmail.com>
2024-08-26 13:23:18 +0300

    add : CountryRepository

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/CountryRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/CountryRepository.java
@@ -1,7 +1,11 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface CountryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Country;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Long> {
 
 
 }

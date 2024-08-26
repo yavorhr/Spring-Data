dc4cf02019ec3f09ab8272aee936a921b6b3a10b
wolfie <yavorhristozov@gmail.com>
2024-08-26 15:44:18 +0300

    update : PersonRepository

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/PersonRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/PersonRepository.java
@@ -7,4 +7,7 @@ import softuni.exam.models.entity.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person,Long> {
 
+  Person findByFirstName(String firstName);
+  Person findByEmail(String email);
+  Person findByPhone(String phone);
 }

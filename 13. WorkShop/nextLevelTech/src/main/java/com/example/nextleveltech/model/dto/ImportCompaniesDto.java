9c2cbebe4e957210b1ec99a67d445a62d1cf9b64
wolfie <yavorhristozov@gmail.com>
2024-08-26 11:20:56 +0300

    add : Import dtos

---
+++ 13. WorkShop/nextLevelTech/src/main/java/com/example/nextleveltech/model/dto/ImportCompaniesDto.java
@@ -0,0 +1,14 @@
+package com.example.nextleveltech.model.dto;
+
+public class ImportCompaniesDto {
+
+  private String companies;
+
+  public String getCompanies() {
+    return companies;
+  }
+
+  public void setCompanies(String companies) {
+    this.companies = companies;
+  }
+}

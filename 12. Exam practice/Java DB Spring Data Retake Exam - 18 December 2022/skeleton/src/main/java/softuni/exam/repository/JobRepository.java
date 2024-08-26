b9da932ae982874d4e28bfe72686f1ca5ed1ad3c
wolfie <yavorhristozov@gmail.com>
2024-08-26 13:23:31 +0300

    add : JobRepository

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/JobRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/JobRepository.java
@@ -1,7 +1,11 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface JobRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Job;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job,Long> {
 
 
 }

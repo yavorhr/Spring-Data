d6c20e46ed74bb0a43f4b91f22cbf2fa2eb07292
wolfie <yavorhristozov@gmail.com>
2024-08-27 11:51:33 +0300

    update : JobRepository
    add export query

--- 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/JobRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 18 December 2022/skeleton/src/main/java/softuni/exam/repository/JobRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.Job;
 
+import java.util.List;
+
 @Repository
 public interface JobRepository extends JpaRepository<Job,Long> {
 
-
+    List<Job> findAllBySalaryGreaterThanEqualAndHoursAweekLessThanEqualOrderBySalaryDesc(Double salary, Double hoursAweek);
 }

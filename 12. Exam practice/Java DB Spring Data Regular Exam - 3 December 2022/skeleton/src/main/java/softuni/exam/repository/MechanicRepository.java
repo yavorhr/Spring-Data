07ab32fb8f51d67f75678da6cd67b5115b9e47e1
wolfie <yavorhristozov@gmail.com>
2024-08-29 11:30:41 +0300

    Update : MechanicRepository
    Add  findByEmail(String email) and findByFirstName(String firstName);

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/repository/MechanicRepository.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/repository/MechanicRepository.java
@@ -4,8 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.Mechanic;
 
+import java.util.Optional;
+
 @Repository
 public interface MechanicRepository extends JpaRepository<Mechanic, Long> {
 
+  Optional<Mechanic> findByEmail(String email);
 
+  Optional<Mechanic> findByFirstName(String firstName);
 }

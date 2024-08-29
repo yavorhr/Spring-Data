2eb402b9fc86babdfc3364e1e0ec58c5ee652e86
wolfie <yavorhristozov@gmail.com>
2024-08-29 15:28:44 +0300

    Update ApplicationBeanConfiguration.java

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -2,20 +2,32 @@ package softuni.exam.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class ApplicationBeanConfiguration {
 
   @Bean
   public ModelMapper modelMapper() {
-    return new ModelMapper();
+    ModelMapper modelMapper = new ModelMapper();
+    modelMapper.addConverter(new Converter<String, LocalDateTime>() {
+      @Override
+      public LocalDateTime convert(MappingContext<String, LocalDateTime> mappingContext) {
+        return LocalDateTime.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+      }
+    });
+    return modelMapper;
   }
 
   @Bean
-  public Gson gson(){
+  public Gson gson() {
     return new GsonBuilder()
             .setPrettyPrinting()
             .create();

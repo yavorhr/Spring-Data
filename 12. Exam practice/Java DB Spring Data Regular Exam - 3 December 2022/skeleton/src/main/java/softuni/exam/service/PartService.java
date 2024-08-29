a98b9ac39e79b96a6bd7117f078e2ab80d0ea8bb
wolfie <yavorhristozov@gmail.com>
2024-08-29 15:27:27 +0300

    Update : PartService
    Add findPartById(Long id);

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/PartService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/PartService.java
@@ -1,12 +1,16 @@
 package softuni.exam.service;
 
+import softuni.exam.models.entity.Part;
+
 import java.io.IOException;
 
 public interface PartService {
 
-    boolean areImported();
+  boolean areImported();
 
-    String readPartsFileContent() throws IOException;
+  String readPartsFileContent() throws IOException;
 
-    String importParts() throws IOException;
+  String importParts() throws IOException;
+
+  Part findPartById(Long id);
 }

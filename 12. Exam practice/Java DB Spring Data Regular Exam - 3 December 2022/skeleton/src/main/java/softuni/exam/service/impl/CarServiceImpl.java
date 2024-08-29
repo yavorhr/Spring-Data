05cf268e1d3b409a7e3825117c310745bef6af6b
wolfie <yavorhristozov@gmail.com>
2024-08-29 12:09:36 +0300

    Add : Feature for persisting Cars to DB

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/impl/CarServiceImpl.java
@@ -1,27 +1,81 @@
 package softuni.exam.service.impl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.json.MechanicSeedDto;
+import softuni.exam.models.dto.xml.CarsRootDto;
+import softuni.exam.models.entity.Car;
+import softuni.exam.models.entity.Mechanic;
+import softuni.exam.repository.CarRepository;
 import softuni.exam.service.CarService;
+import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.XmlParser;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CarServiceImpl implements CarService {
-    private static String CARS_FILE_PATH = "";
+  private static final String CARS_FILE_PATH = "src/main/resources/files/xml/cars.xml";
+  private final CarRepository carRepository;
+  private final XmlParser xmlParser;
+  private final ModelMapper modelMapper;
+  private final ValidationUtil validationUtil;
 
-    @Override
-    public boolean areImported() {
-        return false;
-    }
+  public CarServiceImpl(CarRepository carRepository, XmlParser xmlParser, ModelMapper modelMapper, ValidationUtil validationUtil) {
+    this.carRepository = carRepository;
+    this.xmlParser = xmlParser;
+    this.modelMapper = modelMapper;
+    this.validationUtil = validationUtil;
+  }
 
-    @Override
-    public String readCarsFromFile() throws IOException {
-        return null;
-    }
+  @Override
+  public boolean areImported() {
+    return this.carRepository.count() > 0;
+  }
 
-    @Override
-    public String importCars() throws IOException, JAXBException {
-        return null;
-    }
+  @Override
+  public String readCarsFromFile() throws IOException {
+    return Files.readString(Path.of(CARS_FILE_PATH));
+  }
+
+  @Override
+  public String importCars() throws IOException, JAXBException {
+    StringBuilder sb = new StringBuilder();
+
+    List<String> plateNumbers = new ArrayList<>();
+
+    CarsRootDto carsRootDto = this.xmlParser.fromFile(CARS_FILE_PATH, CarsRootDto.class);
+
+    carsRootDto.getCars()
+            .stream()
+            .filter(dto -> {
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              if (isValid && plateNumbers.contains(dto.getPlateNumber())) {
+                isValid = false;
+              } else {
+                plateNumbers.add(dto.getPlateNumber());
+              }
+
+              sb.append(isValid
+                      ? String.format("Successfully imported car %s - %s",
+                      dto.getMake(),
+                      dto.getModel())
+                      : "Invalid car")
+                      .append(System.lineSeparator());
+
+              return isValid;
+            })
+            .map(dto -> this.modelMapper.map(dto, Car.class))
+            .forEach(this.carRepository::save);
+
+    System.out.println();
+    return sb.toString().trim();
+  }
 }

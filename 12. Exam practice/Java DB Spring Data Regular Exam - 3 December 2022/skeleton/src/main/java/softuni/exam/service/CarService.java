38eacb70275c83ea44cb08bc4c9b94ec22a2a665
wolfie <yavorhristozov@gmail.com>
2024-08-29 11:31:07 +0300

    Refact : Remove comments
    Remove TODO from Services

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/CarService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/CarService.java
@@ -3,9 +3,7 @@ package softuni.exam.service;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
-import java.util.Optional;
 
-// TODO: Implement all methods
 public interface CarService {
 
     boolean areImported();

032bf08ddcc3e4f5264c94a12af9c33d8d7bc798
wolfie <yavorhristozov@gmail.com>
2024-08-29 15:28:32 +0300

    Update CarService.java

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/CarService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/CarService.java
@@ -1,14 +1,18 @@
 package softuni.exam.service;
 
 
+import softuni.exam.models.entity.Car;
+
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
 
 public interface CarService {
 
-    boolean areImported();
+  boolean areImported();
 
-    String readCarsFromFile() throws IOException;
+  String readCarsFromFile() throws IOException;
 
-    String importCars() throws IOException, JAXBException;
+  String importCars() throws IOException, JAXBException;
+
+  Car findCarById(Long id);
 }

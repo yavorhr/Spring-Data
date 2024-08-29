b8109d9a5d89f1780ad1260a27e3785d0071a3e4
wolfie <yavorhristozov@gmail.com>
2024-08-29 11:18:43 +0300

    Update : PartServiceImpl
    Implement class methods

--- 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/impl/PartServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 3 December 2022/skeleton/src/main/java/softuni/exam/service/impl/PartServiceImpl.java
@@ -1,23 +1,71 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.json.PartSeedDto;
+import softuni.exam.models.entity.Part;
+import softuni.exam.repository.PartRepository;
 import softuni.exam.service.PartService;
+import softuni.exam.util.ValidationUtil;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PartServiceImpl implements PartService {
-    @Override
-    public boolean areImported() {
-        return false;
-    }
+  private static final String PARTS_FILE_PATH = "src/main/resources/files/json/parts.json";
+  private final PartRepository partRepository;
+  private final Gson gson;
+  private final ModelMapper modelMapper;
+  private final ValidationUtil validationUtil;
 
-    @Override
-    public String readPartsFileContent() throws IOException {
-        return null;
-    }
+  public PartServiceImpl(PartRepository partRepository, Gson gson, ModelMapper modelMapper, ValidationUtil validationUtil) {
+    this.partRepository = partRepository;
+    this.gson = gson;
+    this.modelMapper = modelMapper;
+    this.validationUtil = validationUtil;
+  }
 
-    @Override
-    public String importParts() throws IOException {
-        return null;
-    }
+  @Override
+  public boolean areImported() {
+    return this.partRepository.count() > 0;
+  }
+
+  @Override
+  public String readPartsFileContent() throws IOException {
+    return Files.readString(Path.of(PARTS_FILE_PATH));
+  }
+
+  @Override
+  public String importParts() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    System.out.println(readPartsFileContent());
+
+    Arrays.stream(this.gson.fromJson(readPartsFileContent(), PartSeedDto[].class))
+            .filter(dto -> {
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              if (isValid && this.partRepository.findByPartName(dto.getPartName()).isPresent()) {
+                isValid = false;
+              }
+
+              sb.append(isValid
+                      ? String.format("Successfully imported part %s - %,2f",
+                      dto.getPartName(),
+                      dto.getPrice())
+                      : "Invalid part")
+                      .append(System.lineSeparator());
+
+              return isValid;
+            })
+            .map(dto -> this.modelMapper.map(dto, Part.class))
+            .forEach(this.partRepository::save);
+
+    return sb.toString().trim();
+  }
 }

5e0378e94b5044d552c242f9ea4c742341c8a654
wolfie <yavorhristozov@gmail.com>
2024-09-01 17:12:02 +0300

    Create ApartmentDto.java

---
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/models/dto/xml/ApartmentDto.java
@@ -0,0 +1,49 @@
+package softuni.exam.models.dto.xml;
+
+import softuni.exam.models.entity.ApartmentTypeEnum;
+import softuni.exam.models.entity.Town;
+
+import javax.persistence.Enumerated;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "apartment")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ApartmentDto {
+
+  private ApartmentTypeEnum apartmentType;
+  private Double area;
+  private Town town;
+
+  @NotNull
+  @Enumerated
+  public ApartmentTypeEnum getApartmentType() {
+    return apartmentType;
+  }
+
+  @NotNull
+  @Min(40)
+  public Double getArea() {
+    return area;
+  }
+
+  public Town getTown() {
+    return town;
+  }
+
+  public void setApartmentType(ApartmentTypeEnum apartmentType) {
+    this.apartmentType = apartmentType;
+  }
+
+  public void setArea(Double area) {
+    this.area = area;
+  }
+
+  public void setTown(Town town) {
+    this.town = town;
+  }
+}

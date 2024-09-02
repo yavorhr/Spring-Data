be1c74a0f93e4bb242499eb7be9c3b9649c13834
wolfie <yavorhristozov@gmail.com>
2024-09-02 14:02:31 +0300

    Update ApartmentRepository.java

--- 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/ApartmentRepository.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/ApartmentRepository.java
@@ -3,7 +3,6 @@ package softuni.exam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.Apartment;
-import softuni.exam.models.entity.ApartmentTypeEnum;
 
 @Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {

87fe8b8c48cf676eb7d1b089c9c2c669fa2c7d71
wolfie <yavorhristozov@gmail.com>
2024-09-02 12:42:24 +0300

    Update ApartmentRepository.java
    add : findByTown_TownNameAndArea(String town_townName, Double area);

--- 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/ApartmentRepository.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/ApartmentRepository.java
@@ -3,8 +3,10 @@ package softuni.exam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.Apartment;
+import softuni.exam.models.entity.ApartmentTypeEnum;
 
 @Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 
+  Apartment findByTown_TownNameAndArea(String town_townName, Double area);
 }

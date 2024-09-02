685dba65731fcab6b4ee90d1e40658dd3932b057
wolfie <yavorhristozov@gmail.com>
2024-09-02 14:02:26 +0300

    Update OfferRepository.java
    Add query for extracting Offers for the last task

--- 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/OfferRepository.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 2 April 2022/skeleton/src/main/java/softuni/exam/repository/OfferRepository.java
@@ -2,9 +2,13 @@ package softuni.exam.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.ApartmentTypeEnum;
 import softuni.exam.models.entity.Offer;
+
+import java.util.List;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer, Long> {
 
+  List<Offer> findAllByApartment_ApartmentTypeOrderByApartment_AreaDescPriceAsc(ApartmentTypeEnum apartment_apartmentType);
 }

eae84299753cffbb5d4402fa60516537481bf843
wolfie <yavorhristozov@gmail.com>
2024-09-03 15:05:28 +0300

    Update ApplicationBeanConfiguration.java
    Add @Bean for ModelMapper

--- 12. Exam practice/Java DB Spring Data Retake Exam - 17 April 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 17 April 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -2,17 +2,23 @@ package softuni.exam.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationBeanConfiguration {
-  
+
   @Bean
   public Gson gson() {
     return new GsonBuilder()
             .setPrettyPrinting()
             .create();
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
   }
 
 }

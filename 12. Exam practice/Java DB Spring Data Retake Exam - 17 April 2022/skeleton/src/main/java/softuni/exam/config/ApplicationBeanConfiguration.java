cf5c00c7d5f7cc8fdbb0be4773884e482cf85a78
wolfie <yavorhristozov@gmail.com>
2024-09-03 15:04:33 +0300

    Update ApplicationBeanConfiguration.java
    add @Bean for GSon

--- 12. Exam practice/Java DB Spring Data Retake Exam - 17 April 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 17 April 2022/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,18 @@
 package softuni.exam.config;
 
-// TODO:
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ApplicationBeanConfiguration {
+  
+  @Bean
+  public Gson gson() {
+    return new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+  }
 
 }

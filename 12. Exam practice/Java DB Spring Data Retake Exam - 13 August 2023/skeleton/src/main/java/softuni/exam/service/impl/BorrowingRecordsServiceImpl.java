165118c9c72f166458682922be3d126665ba0d0a
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:47:58 +0300

    Add : BorrowingRecordsServiceImpl class as a service implementation
    - Created BorrowingRecordsServiceImpl class to implement BorrowingRecordsService interface.
    - Annotated with @Service for Spring's service layer component scanning.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/BorrowingRecordsServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/BorrowingRecordsServiceImpl.java
@@ -1,6 +1,31 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class BorrowingRecordsServiceImpl {
+import org.springframework.stereotype.Service;
+import softuni.exam.service.BorrowingRecordsService;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
+@Service
+public class BorrowingRecordsServiceImpl implements BorrowingRecordsService {
+
+  @Override
+  public boolean areImported() {
+    return false;
+  }
+
+  @Override
+  public String readBorrowingRecordsFromFile() throws IOException {
+    return null;
+  }
+
+  @Override
+  public String importBorrowingRecords() throws IOException, JAXBException {
+    return null;
+  }
+
+  @Override
+  public String exportBorrowingRecords() {
+    return null;
+  }
 }

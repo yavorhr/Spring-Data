48d5139cb89a2af07775ea99b140cb76cc0c0341
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:49:44 +0300

    Add LibraryMemberServiceImpl class as a service implementation
    - Created LibraryMemberServiceImpl class to implement LibraryMemberService interface.
    - Annotated with @Service for Spring's service layer component scanning.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
@@ -1,6 +1,25 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class LibraryMemberServiceImpl{
+import org.springframework.stereotype.Service;
+import softuni.exam.service.LibraryMemberService;
 
+import java.io.IOException;
+
+@Service
+public class LibraryMemberServiceImpl implements LibraryMemberService {
+
+  @Override
+  public boolean areImported() {
+    return false;
+  }
+
+  @Override
+  public String readLibraryMembersFileContent() throws IOException {
+    return null;
+  }
+
+  @Override
+  public String importLibraryMembers() throws IOException {
+    return null;
+  }
 }

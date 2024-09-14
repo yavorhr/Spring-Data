aa13629f345e39abf2cd646999898f235f86c0d4
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:50:56 +0300

    Add : BookRepository for managing Book entities
    - Introduced BookRepository extending JpaRepository to handle CRUD operations for Book entities.
    - Annotated with @Repository for Spring Data JPA integration.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BookRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BookRepository.java
@@ -1,6 +1,10 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
 
 }

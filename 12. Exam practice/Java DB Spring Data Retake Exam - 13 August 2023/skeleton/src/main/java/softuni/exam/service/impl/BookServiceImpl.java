bec11bf4d9fa56c121b996454f283d5419ad20da
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:46:46 +0300

    Add BookServiceImpl  class as a service implementation
    - Created BookServiceImpl  class to implement BookService interface.
    - Annotated with @Service for Spring's service layer component scanning.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/BookServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/BookServiceImpl.java
@@ -1,7 +1,25 @@
 package softuni.exam.service.impl;
 
-// TODO: Implement all methods
-public class BookServiceImpl {
+import org.springframework.stereotype.Service;
+import softuni.exam.service.BookService;
 
+import java.io.IOException;
 
+@Service
+public class BookServiceImpl implements BookService {
+
+  @Override
+  public boolean areImported() {
+    return false;
+  }
+
+  @Override
+  public String readBooksFromFile() throws IOException {
+    return null;
+  }
+
+  @Override
+  public String importBooks() throws IOException {
+    return null;
+  }
 }

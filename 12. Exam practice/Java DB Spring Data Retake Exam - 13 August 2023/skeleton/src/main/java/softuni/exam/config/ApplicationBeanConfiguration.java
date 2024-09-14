89fb14317eae5703dc761d4863736802b5a0b6ca
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:57:50 +0300

    Update ApplicationBeanConfiguration.java
    - Add @Bean for ModelMapper library

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,6 +1,14 @@
 package softuni.exam.config;
 
-// TODO:
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApplicationBeanConfiguration {
 
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
 }

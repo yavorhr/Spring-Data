67a2399c46a71eb99c50fc5853b2a31e895b041d
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:58:49 +0300

    Update ApplicationBeanConfiguration.java
    - Add @Bean for Gson library

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -1,5 +1,7 @@
 package softuni.exam.config;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,12 @@ public class ApplicationBeanConfiguration {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public Gson gson(){
+    return new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
   }
 }

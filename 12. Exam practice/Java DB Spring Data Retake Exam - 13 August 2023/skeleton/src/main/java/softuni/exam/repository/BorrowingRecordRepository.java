b89b291d6c06dac0ddbe01d5b5116e844d0e5513
wolfie <yavorhristozov@gmail.com>
2024-09-14 15:52:37 +0300

    Update : BorrowingRecordRepository for managing BorrowingRecord entities
    - Introduced BorrowingRecordRepository extending JpaRepository to handle CRUD operations for BorrowingRecord entities.
    - Annotated with @Repository for Spring Data JPA integration.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
@@ -1,6 +1,10 @@
 package softuni.exam.repository;
 
-// TODO:
-public interface BorrowingRecordRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import softuni.exam.models.entity.BorrowingRecord;
+
+@Repository
+public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
 
 }

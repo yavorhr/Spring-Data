116e61375167c98256fd8160745b3360cad1ba31
wolfie <yavorhristozov@gmail.com>
2024-09-15 11:19:43 +0300

    Update : LibraryMemberServiceImpl
    - Implement all method's signatures, which are implemented by the interface
    - importLibraryMembers() is the primary method in the class. It reads all members from JSON file and seeds them in the databse after validating them.  The method returns the result as String in the console.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/service/impl/LibraryMemberServiceImpl.java
@@ -1,25 +1,66 @@
 package softuni.exam.service.impl;
 
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.models.dto.json.MemberSeedDto;
+import softuni.exam.models.entity.LibraryMember;
+import softuni.exam.repository.LibraryMemberRepository;
 import softuni.exam.service.LibraryMemberService;
+import softuni.exam.util.ValidationUtil;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 @Service
 public class LibraryMemberServiceImpl implements LibraryMemberService {
+  private static final String LIB_MEMBERS_FILE_PATH = "src/main/resources/files/json/library-members.json";
+  private final LibraryMemberRepository libraryMemberRepository;
+  private final ModelMapper modelMapper;
+  private final Gson gson;
+  private final ValidationUtil validationUtil;
+
+  public LibraryMemberServiceImpl(LibraryMemberRepository libraryMemberRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validationUtil) {
+    this.libraryMemberRepository = libraryMemberRepository;
+    this.modelMapper = modelMapper;
+    this.gson = gson;
+    this.validationUtil = validationUtil;
+  }
 
   @Override
   public boolean areImported() {
-    return false;
+    return this.libraryMemberRepository.count() > 0;
   }
 
   @Override
   public String readLibraryMembersFileContent() throws IOException {
-    return null;
+    return Files.readString(Path.of(LIB_MEMBERS_FILE_PATH));
   }
 
   @Override
   public String importLibraryMembers() throws IOException {
-    return null;
+    StringBuilder sb = new StringBuilder();
+
+    Arrays.stream(this.gson.fromJson(readLibraryMembersFileContent(), MemberSeedDto[].class))
+            .filter(dto -> {
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              if (this.libraryMemberRepository.findByPhoneNumber(dto.getPhoneNumber()).isPresent()) {
+                isValid = false;
+              }
+
+              sb.append(isValid
+                      ? String.format("Successfully imported library member %s - %s", dto.getFirstName(), dto.getLastName())
+                      : "Invalid library member")
+                      .append(System.lineSeparator());
+
+              return isValid;
+            })
+            .map(dto -> this.modelMapper.map(dto, LibraryMember.class))
+            .forEach(this.libraryMemberRepository::save);
+
+    return sb.toString();
   }
 }

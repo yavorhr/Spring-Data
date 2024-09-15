fa347be3b017f64cd9c3fe1973345c71403acb73
wolfie <yavorhristozov@gmail.com>
2024-09-15 15:47:30 +0300

    Update BorrowingRecordRepository.java
    Add query for extracting borrowing records with the following criteria :
    - before  borrow date
    - per book genre
    - ordered by borrow date desc

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2023/skeleton/src/main/java/softuni/exam/repository/BorrowingRecordRepository.java
@@ -3,8 +3,14 @@ package softuni.exam.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.models.entity.BorrowingRecord;
+import softuni.exam.models.entity.enums.GenreEnum;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Repository
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
+
+  Set<BorrowingRecord> findAllByBorrowDateBeforeAndBook_GenreOrderByBorrowDateDesc(LocalDate borrowDate, GenreEnum bookGenre);
 
 }

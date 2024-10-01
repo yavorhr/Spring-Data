d9ba91d6abfc80a8ec5f7027d5087f3645a4093f
wolfie <yavorhristozov@gmail.com>
2024-10-01 15:13:16 +0300

    Add : CustomerRepository for managing Customer entities
    - Introduced CustomerRepository extending JpaRepository to handle CRUD operations for Customer entities.
    - Annotated with @Repository for Spring Data JPA integration.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/CustomerRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package exam.repository;
 
+import exam.model.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-public interface CustomerRepository  {
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }

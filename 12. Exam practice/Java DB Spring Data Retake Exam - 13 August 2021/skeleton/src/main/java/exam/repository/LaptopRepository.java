56f582c4f016b4a2d6482eb66d2b4827c4b66c33
wolfie <yavorhristozov@gmail.com>
2024-10-01 15:13:42 +0300

    Add : LaptopRepository for managing Laptop entities
    - Introduced LaptopRepository extending JpaRepository to handle CRUD operations for Laptop entities.
    - Annotated with @Repository for Spring Data JPA integration.

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/LaptopRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/LaptopRepository.java
@@ -1,5 +1,9 @@
 package exam.repository;
 
-//ToDo:
-public interface LaptopRepository {
+import exam.model.entity.Laptop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LaptopRepository extends JpaRepository<Laptop,Long> {
 }

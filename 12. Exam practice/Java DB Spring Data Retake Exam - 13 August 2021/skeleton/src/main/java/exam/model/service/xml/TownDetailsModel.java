b27c749cefd8a6f3326971c3097ebd8b82b2ce6b
wolfie <yavorhristozov@gmail.com>
2024-10-02 18:51:28 +0300

    Update TownDetailsModel.java
    Add @XmlElement annotations

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/model/service/xml/TownDetailsModel.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/model/service/xml/TownDetailsModel.java
@@ -5,13 +5,17 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "town")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class TownDetailsModel {
+  @XmlElement
   private String name;
+  @XmlElement
   private Integer population;
+  @XmlElement(name = "travel-guide")
   private String travelGuide;
 
   public TownDetailsModel() {

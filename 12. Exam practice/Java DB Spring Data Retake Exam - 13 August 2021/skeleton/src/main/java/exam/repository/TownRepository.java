4468edb8396e7bc4b533b92cbb8bdc031939e7ca
wolfie <yavorhristozov@gmail.com>
2024-10-02 18:51:44 +0300

    Update TownRepository.java
    Add findByName(String name) method

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/TownRepository.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/repository/TownRepository.java
@@ -4,6 +4,10 @@ import exam.model.entity.Town;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TownRepository extends JpaRepository<Town, Long> {
+
+  Optional<Town> findByName(String name);
 }

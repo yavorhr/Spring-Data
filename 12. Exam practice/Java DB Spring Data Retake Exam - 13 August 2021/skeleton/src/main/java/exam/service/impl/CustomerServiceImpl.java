f08b794f6d8ff910ec2ee6f5af4b5171bc6dd735
wolfie <yavorhristozov@gmail.com>
2024-10-03 14:53:40 +0300

    Update : CustomerServiceImpl
    - Implement the interface methods from the service
    - Added dependencies, which are mainly used for the main method from the service - importCustomers()
    - importCustomers() does the following :
    * deserialize JSON data to a Dto java pojo calss using the GSON dependency
    * validates the information using the validationUtil class and the jakarta validation annotations in the Dto object
    * maps the Dto to entity class
    * persist the entity to the db
    * returns String result to the console

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/service/impl/CustomerServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/service/impl/CustomerServiceImpl.java
@@ -1,25 +1,81 @@
 package exam.service.impl;
 
+import com.google.gson.Gson;
+import exam.model.entity.Customer;
+import exam.model.entity.Town;
+import exam.model.service.json.CustomerDetailsModel;
+import exam.repository.CustomerRepository;
 import exam.service.CustomerService;
+import exam.service.TownService;
+import exam.util.ValidationUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+  private static final String CUSTOMERS_FILE_PATH = "src/main/resources/files/json/customers.json";
+  private final CustomerRepository customerRepository;
+  private final ModelMapper modelMapper;
+  private final Gson gson;
+  private final ValidationUtil validationUtil;
+  private final TownService townService;
+
+  public CustomerServiceImpl(CustomerRepository customerRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validationUtil, TownService townService) {
+    this.customerRepository = customerRepository;
+    this.modelMapper = modelMapper;
+    this.gson = gson;
+    this.validationUtil = validationUtil;
+    this.townService = townService;
+  }
 
   @Override
   public boolean areImported() {
-    return false;
+    return this.customerRepository.count() > 0;
   }
 
   @Override
   public String readCustomersFileContent() throws IOException {
-    return null;
+    return Files.readString(Path.of(CUSTOMERS_FILE_PATH));
   }
 
   @Override
   public String importCustomers() throws IOException {
-    return null;
+    StringBuilder sb = new StringBuilder();
+    Arrays.stream(this.gson.fromJson(readCustomersFileContent(), CustomerDetailsModel[].class))
+            .filter(dto -> {
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              if (customerEmailExists(dto)) {
+                isValid = false;
+              }
+
+              sb.append(isValid
+                      ? String.format("Successfully imported Customer %s %s - %s",
+                      dto.getFirstName(),
+                      dto.getLastName(),
+                      dto.getEmail())
+                      : "Invalid Customer")
+                      .append(System.lineSeparator());
+
+              return isValid;
+            }).map(dto -> {
+      Customer entity = this.modelMapper.map(dto, Customer.class);
+
+      entity.setTown(this.townService.findTownByName(dto.getTown().getName()));
+
+      return entity;
+    }).forEach(this.customerRepository::save);
+
+
+    return sb.toString().trim();
+  }
+
+  private boolean customerEmailExists(CustomerDetailsModel dto) {
+    return this.customerRepository.findByEmail(dto.getEmail()).isPresent();
   }
 }

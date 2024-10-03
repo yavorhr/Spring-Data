653ef72d5a97058660e8c90075b24e8c592f4a2e
wolfie <yavorhristozov@gmail.com>
2024-10-03 15:24:51 +0300

    Update : LaptopServiceImpl
    - Implement the interface methods from the service
    - Added dependencies, which are mainly used for the main method from the service - importLaptops()
    - importShops() does the following :
    * deserialize JSON data to a Dto java pojo calss using the GSON dependency
    * validates the information using the validationUtil class and the jakarta validation annotations in the Dto object
    * maps the Dto to entity class
    * persist the entity to the db
    * returns String result to the console

--- 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/service/impl/LaptopServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Retake Exam - 13 August 2021/skeleton/src/main/java/exam/service/impl/LaptopServiceImpl.java
@@ -1,29 +1,89 @@
 package exam.service.impl;
 
+import com.google.gson.Gson;
+import exam.model.entity.Customer;
+import exam.model.entity.Laptop;
+import exam.model.service.json.CustomerDetailsModel;
+import exam.model.service.json.LaptopDetailsModel;
+import exam.repository.LaptopRepository;
 import exam.service.LaptopService;
+import exam.service.ShopService;
+import exam.util.ValidationUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 @Service
 public class LaptopServiceImpl implements LaptopService {
+  private static final String LAPTOPS_FILE_PATH = "src/main/resources/files/json/laptops.json";
+  private final LaptopRepository laptopRepository;
+  private final ModelMapper modelMapper;
+  private final Gson gson;
+  private final ValidationUtil validationUtil;
+  private final ShopService shopService;
+
+  public LaptopServiceImpl(LaptopRepository laptopRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validationUtil, ShopService shopService) {
+    this.laptopRepository = laptopRepository;
+    this.modelMapper = modelMapper;
+    this.gson = gson;
+    this.validationUtil = validationUtil;
+    this.shopService = shopService;
+  }
+
   @Override
   public boolean areImported() {
-    return false;
+    return this.laptopRepository.count() > 0;
   }
 
   @Override
   public String readLaptopsFileContent() throws IOException {
-    return null;
+    return Files.readString(Path.of(LAPTOPS_FILE_PATH));
   }
 
   @Override
   public String importLaptops() throws IOException {
-    return null;
+    StringBuilder sb = new StringBuilder();
+
+    Arrays.stream(this.gson.fromJson(readLaptopsFileContent(), LaptopDetailsModel[].class))
+            .filter(dto -> {
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              if (macAddressAlreadyExists(dto)) {
+                isValid = false;
+              }
+
+              sb.append(isValid
+                      ? String.format("Successfully imported Laptop %s - %.2f - %d - %d",
+                      dto.getMacAddress(),
+                      dto.getCpuSpeed(),
+                      dto.getRam(),
+                      dto.getStorage())
+                      : "Invalid Laptop")
+                      .append(System.lineSeparator());
+
+              return isValid;
+            }).map(dto -> {
+      Laptop entity = this.modelMapper.map(dto, Laptop.class);
+
+      entity.setShop(this.shopService.findShopByName(dto.getShop().getName()));
+
+      return entity;
+    })
+            .forEach(this.laptopRepository::save);
+
+    return sb.toString().trim();
   }
 
   @Override
-  public String exportBestLaptops() {
-    return null;
+  public String exportBestLaptops() throws IOException {
+    return "";
+  }
+
+  private boolean macAddressAlreadyExists(LaptopDetailsModel dto) {
+    return this.laptopRepository.findByMacAddress(dto.getMacAddress()).isPresent();
   }
 }

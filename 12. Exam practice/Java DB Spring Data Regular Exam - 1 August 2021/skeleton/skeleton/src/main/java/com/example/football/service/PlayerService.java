8a582bab1ec143cde929fc22056b0b664df66b58
wolfie <yavorhristozov@gmail.com>
2024-10-05 14:58:53 +0300

    Add : ServiceImpl classes as a service implementation
    Add @Service annotation on all ServiceImpl classes

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/PlayerService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/PlayerService.java
@@ -1,6 +1,5 @@
 package com.example.football.service;
 
-//ToDo - Implement all methods
 public interface PlayerService {
     boolean areImported();
 
--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/impl/PlayerServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/impl/PlayerServiceImpl.java
@@ -1,28 +1,28 @@
 package com.example.football.service.impl;
 
 import com.example.football.service.PlayerService;
+import org.springframework.stereotype.Service;
 
-//ToDo - Implement all methods
+@Service
 public class PlayerServiceImpl implements PlayerService {
 
+  @Override
+  public boolean areImported() {
+    return false;
+  }
 
-    @Override
-    public boolean areImported() {
-        return false;
-    }
+  @Override
+  public String readPlayersFileContent() {
+    return null;
+  }
 
-    @Override
-    public String readPlayersFileContent()  {
-        return null;
-    }
+  @Override
+  public String importPlayers() {
+    return null;
+  }
 
-    @Override
-    public String importPlayers()  {
-        return null;
-    }
-
-    @Override
-    public String exportBestPlayers() {
-        return null;
-    }
+  @Override
+  public String exportBestPlayers() {
+    return null;
+  }
 }

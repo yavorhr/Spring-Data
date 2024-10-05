4f1ae0c1cf85b741f994b89626ee0a283fab6201
wolfie <yavorhristozov@gmail.com>
2024-10-05 15:51:59 +0300

    Update TownServiceImpl.java
    Implement the methods from the TownService

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/impl/TownServiceImpl.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/impl/TownServiceImpl.java
@@ -1,24 +1,67 @@
 package com.example.football.service.impl;
 
+import com.example.football.models.dto.json.TownDetailsModel;
+import com.example.football.models.entity.Town;
+import com.example.football.repository.TownRepository;
 import com.example.football.service.TownService;
+import com.example.football.util.ValidationUtil;
+import com.google.gson.Gson;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 @Service
 public class TownServiceImpl implements TownService {
+  private static final String TOWNS_FILE_PATH = "src/main/resources/files/json/towns.json";
+  private final TownRepository townRepository;
+  private final ModelMapper modelMapper;
+  private final Gson gson;
+  private final ValidationUtil validationUtil;
 
+  public TownServiceImpl(TownRepository townRepository, ModelMapper modelMapper, Gson gson, ValidationUtil validationUtil) {
+    this.townRepository = townRepository;
+    this.modelMapper = modelMapper;
+    this.gson = gson;
+    this.validationUtil = validationUtil;
+  }
 
-    @Override
-    public boolean areImported() {
-        return false;
-    }
+  @Override
+  public boolean areImported() {
+    return this.townRepository.count() > 0;
+  }
 
-    @Override
-    public String readTownsFileContent() {
-        return null;
-    }
+  @Override
+  public String readTownsFileContent() throws IOException {
+    return Files.readString(Path.of(TOWNS_FILE_PATH)));
+  }
 
-    @Override
-    public String importTowns() {
-        return null;
-    }
+  @Override
+  public String importTowns() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    Arrays.stream(gson.fromJson(readTownsFileContent(), TownDetailsModel[].class))
+            .filter(dto -> {
+              boolean isValid = validationUtil.isValid(dto);
+
+              if (this.townRepository.findByName(dto.getName()).isPresent()) {
+                isValid = false;
+              }
+
+              sb
+                      .append(isValid
+                              ? String.format("Successfully imported Town %s - %d", dto.getName(), dto.getPopulation())
+                              : "Invalid Town")
+                      .append(System.lineSeparator());
+
+              return isValid;
+
+            })
+            .map(dto -> modelMapper.map(dto, Town.class))
+            .forEach(townRepository::save);
+
+    return sb.toString().trim();
+  }
 }

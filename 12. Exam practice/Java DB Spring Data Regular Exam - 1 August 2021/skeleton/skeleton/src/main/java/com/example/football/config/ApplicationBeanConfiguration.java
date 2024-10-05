12653140c84174baa7b39ee112b484d44487da34
wolfie <yavorhristozov@gmail.com>
2024-10-05 14:07:07 +0300

    Update ApplicationBeanConfiguration.java
    Add ModelMapper @Bean

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/config/ApplicationBeanConfiguration.java
@@ -1,5 +1,27 @@
 package com.example.football.config;
 
-//ToDo Create configurations
+import org.modelmapper.Converter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.spi.MappingContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class ApplicationBeanConfiguration {
+
+  @Bean
+  public ModelMapper modelMapper() {
+    ModelMapper modelMapper = new ModelMapper();
+
+//    modelMapper.addConverter(new Converter<String, LocalDate>() {
+//      @Override
+//      public LocalDate convert(MappingContext<String, LocalDate> mappingContext) {
+//        return LocalDate.parse(mappingContext.getSource(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+//      }
+//    });
+    return modelMapper;
+  }
+
 }
+

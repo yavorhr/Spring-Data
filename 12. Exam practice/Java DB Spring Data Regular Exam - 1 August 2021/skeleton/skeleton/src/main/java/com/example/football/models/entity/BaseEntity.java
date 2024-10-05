85c8e64733b6ab81f22a14d0dd26cabb4c8ed41c
wolfie <yavorhristozov@gmail.com>
2024-10-05 14:22:53 +0300

    Add : BaseEntity class
    - annotated with @MappedSuperClass to mark the class as no persistent for Spring
    - has field Long id and it will be inherited from the rest of the classes in order to receive the Id as well
    - add getter() and setter() for the id

---
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/models/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.example.football.models.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+  private Long id;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long getId() {
+    return id;
+  }
+
+  public BaseEntity setId(Long id) {
+    this.id = id;
+    return this;
+  }
+}

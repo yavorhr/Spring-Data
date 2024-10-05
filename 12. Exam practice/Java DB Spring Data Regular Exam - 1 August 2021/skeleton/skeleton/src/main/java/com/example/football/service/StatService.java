8ae23da770b15fe5ac6170eb58a990c1170d9a1a
wolfie <yavorhristozov@gmail.com>
2024-10-05 16:07:48 +0300

    Update StatService.java
    add IO exception on readStatsFileContent()

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/StatService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/StatService.java
@@ -1,9 +1,11 @@
 package com.example.football.service;
 
+import java.io.IOException;
+
 public interface StatService {
     boolean areImported();
 
-    String readStatsFileContent() ;
+    String readStatsFileContent() throws IOException;
 
     String importStats() ;
 

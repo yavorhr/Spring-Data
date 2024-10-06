04992e66287a9d8846064b8eeb20df763a77b4de
wolfie <yavorhristozov@gmail.com>
2024-10-06 14:59:28 +0300

    Update Picture.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Picture.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Picture.java
@@ -1,5 +1,24 @@
 package softuni.exam.domain.entities;
 
-public class Picture {
-    //TODO
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pictures")
+public class Picture extends BaseEntity {
+    private String url;
+
+  public Picture() {
+  }
+
+  @Column(nullable = false,unique = true)
+  public String getUrl() {
+    return url;
+  }
+
+  public Picture setUrl(String url) {
+    this.url = url;
+    return this;
+  }
 }

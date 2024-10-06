8414dd52f33492259952c7a91ff250aa23db4d47
wolfie <yavorhristozov@gmail.com>
2024-10-06 13:43:33 +0300

    Update PlayerService.java
    Add exception signatures

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/PlayerService.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/service/PlayerService.java
@@ -1,12 +1,15 @@
 package com.example.football.service;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
 public interface PlayerService {
 
     boolean areImported();
 
-    String readPlayersFileContent() ;
+    String readPlayersFileContent() throws IOException;
 
-    String importPlayers() ;
+    String importPlayers() throws JAXBException;
 
-    String exportBestPlayers();
+    String exportBestPlayers() throws JAXBException;
 }

4fa6cf556a6e8f131a13bbe3f27c0fe7f239efb1
wolfie <yavorhristozov@gmail.com>
2024-10-06 14:59:34 +0300

    Update Team.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Team.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Team.java
@@ -1,5 +1,36 @@
 package softuni.exam.domain.entities;
 
-public class Team {
-    //TODO
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "teams")
+public class Team extends BaseEntity {
+    private String name;
+    private Picture picture;
+
+  public Team() {
+  }
+
+  @Column(unique = true, nullable = false)
+  public String getName() {
+    return name;
+  }
+
+  @ManyToOne
+  public Picture getPicture() {
+    return picture;
+  }
+
+  public Team setName(String name) {
+    this.name = name;
+    return this;
+  }
+
+  public Team setPicture(Picture picture) {
+    this.picture = picture;
+    return this;
+  }
 }

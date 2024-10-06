74dbc06ae456ddead92bd27504784268ed3ce68a
wolfie <yavorhristozov@gmail.com>
2024-10-06 17:53:46 +0300

    Update PictureServiceImpl.java
    Implement the @override methods from the service interface

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/service/PictureServiceImpl.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/service/PictureServiceImpl.java
@@ -1,26 +1,64 @@
 package softuni.exam.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import softuni.exam.domain.entities.Picture;
+import softuni.exam.domain.models.xml.PicturesRootModel;
+import softuni.exam.repository.PictureRepository;
+import softuni.exam.util.ValidationUtil;
+import softuni.exam.util.XmlParser;
+
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 public class PictureServiceImpl implements PictureService {
+  private static final String PICTURES_FILE_PATH = "src/main/resources/files/xml/pictures.xml";
+  private final ModelMapper modelMapper;
+  private final PictureRepository pictureRepository;
+  private final ValidationUtil validationUtil;
+  private final XmlParser xmlParser;
 
-    @Override
-    public String importPictures() {
-        //TODO Implement me
-        return "";
-    }
+  public PictureServiceImpl(ModelMapper modelMapper, PictureRepository pictureRepository, ValidationUtil validationUtil, XmlParser xmlParser) {
+    this.modelMapper = modelMapper;
+    this.pictureRepository = pictureRepository;
+    this.validationUtil = validationUtil;
+    this.xmlParser = xmlParser;
+  }
 
-    @Override
-    public boolean areImported() {
-        //TODO Implement me
-        return false;
-    }
+  @Override
+  public String importPictures() throws JAXBException {
+    StringBuilder sb = new StringBuilder();
 
-    @Override
-    public String readPicturesXmlFile() {
-        //TODO Implement me
-        return "";
-    }
+    this.xmlParser.fromFile(PICTURES_FILE_PATH, PicturesRootModel.class).getPictures()
+            .stream()
+            .filter(dto -> {
+
+              boolean isValid = this.validationUtil.isValid(dto);
+
+              sb.append(isValid
+                      ? String.format("Successfully imported picture - %s", dto.getUrl())
+                      : "Invalid picture")
+                      .append(System.lineSeparator());
+              return isValid;
+            })
+            .map(dto -> this.modelMapper.map(dto, Picture.class))
+            .forEach(this.pictureRepository::save);
+
+
+    return sb.toString().trim();
+  }
+
+  @Override
+  public boolean areImported() {
+    return this.pictureRepository.count() > 0;
+  }
+
+  @Override
+  public String readPicturesXmlFile() throws IOException {
+    return Files.readString(Path.of(PICTURES_FILE_PATH));
+  }
 
 }

922a40cd61901b5fe6a3725d31940aced4538301
wolfie <yavorhristozov@gmail.com>
2024-10-06 17:46:37 +0300

    Update BaseEntity.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/BaseEntity.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/BaseEntity.java
@@ -1,9 +1,10 @@
 package softuni.exam.domain.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class BaseEntity {

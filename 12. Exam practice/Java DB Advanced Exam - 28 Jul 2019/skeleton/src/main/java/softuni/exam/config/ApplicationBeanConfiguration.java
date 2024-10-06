a68db9b1df7dcc59859c067dc52610b661049117
wolfie <yavorhristozov@gmail.com>
2024-10-06 14:25:57 +0300

    Update ApplicationBeanConfiguration.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/config/ApplicationBeanConfiguration.java
@@ -5,24 +5,21 @@ import com.google.gson.GsonBuilder;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import softuni.exam.util.FileUtil;
-import softuni.exam.util.FileUtilImpl;
 
 @Configuration
 public class ApplicationBeanConfiguration {
 
     @Bean
-    public FileUtil fileUtil() {
-        return new FileUtilImpl();
-    }
-
-    @Bean
     public Gson gson() {
-        return new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();
+        return new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .setPrettyPrinting()
+                .create();
     }
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
 }

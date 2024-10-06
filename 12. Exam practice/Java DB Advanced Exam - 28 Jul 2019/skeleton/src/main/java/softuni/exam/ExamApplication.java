76c756219cdd80840425bbd6ce26f4375186c4bf
wolfie <yavorhristozov@gmail.com>
2024-10-06 17:50:24 +0300

    Update ExamApplication.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/ExamApplication.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/ExamApplication.java
@@ -2,12 +2,13 @@ package softuni.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ExamApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExamApplication.class, args);
     }
-
 }

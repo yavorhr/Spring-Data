31ebc98eee4872fe72b269263f03441c27ee6bc5
wolfie <yavorhristozov@gmail.com>
2024-10-06 13:46:44 +0300

    Update ExportController.java

--- 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/web/controllers/ExportController.java
+++ 12. Exam practice/Java DB Spring Data Regular Exam - 1 August 2021/skeleton/skeleton/src/main/java/com/example/football/web/controllers/ExportController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.xml.bind.JAXBException;
+
 @Controller
 @RequestMapping("/export")
 public class ExportController extends BaseController {
@@ -19,7 +21,7 @@ public class ExportController extends BaseController {
     }
 
     @GetMapping("/best-players")
-    public ModelAndView exportCarsByPictures() {
+    public ModelAndView exportCarsByPictures() throws JAXBException {
         String players = this.playerService
                 .exportBestPlayers();
         return super.view("export/export-best-players", "players", players);

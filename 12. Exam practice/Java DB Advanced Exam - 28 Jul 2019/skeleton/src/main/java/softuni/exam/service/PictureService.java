51861cb2d930ab9a9248a8d2bc53fe2ccdbfd5f3
wolfie <yavorhristozov@gmail.com>
2024-10-06 17:52:44 +0300

    Update PictureService.java
    add JAXBException()

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/service/PictureService.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/service/PictureService.java
@@ -1,9 +1,12 @@
 package softuni.exam.service;
 
+import javax.xml.bind.JAXBException;
+import java.io.IOException;
+
 public interface PictureService {
-    String importPictures();
+    String importPictures() throws JAXBException;
 
     boolean areImported();
 
-    String readPicturesXmlFile();
+    String readPicturesXmlFile() throws IOException;
 }

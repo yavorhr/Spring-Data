c33d0eeb12b94d7bde39ebb28a7212f809929c1a
wolfie <yavorhristozov@gmail.com>
2024-10-07 15:14:11 +0300

    Update PlayerRepository.java
    Add query findAllByTeamNameOrderById(String teamName);

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PlayerRepository.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PlayerRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.domain.entities.Player;
 
+import java.util.List;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Long> {
 
+  List<Player> findAllByTeamNameOrderById(String teamName);
 }

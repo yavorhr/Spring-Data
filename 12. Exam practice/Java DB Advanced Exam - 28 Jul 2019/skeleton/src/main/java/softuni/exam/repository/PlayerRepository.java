ab64e819b8a258d0091c29b47701cf727cc8c062
wolfie <yavorhristozov@gmail.com>
2024-10-07 15:19:53 +0300

    Update PlayerRepository.java
    Add JPQL Query for finding all players with salary > 100000, ordered by Salary Desc

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PlayerRepository.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PlayerRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.domain.entities.Player;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player,Long> {
 
   List<Player> findAllByTeamNameOrderById(String teamName);
+
+  List<Player> findAllBySalaryGreaterThanOrderBySalaryDesc(BigDecimal salary);
 }

73bdb48678e7ab802efef1d4e683950967817fb8
wolfie <yavorhristozov@gmail.com>
2024-10-07 15:14:40 +0300

    Update Team.java
    Add mapped by OneToMany relation with Player entity

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Team.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/domain/entities/Team.java
@@ -1,18 +1,22 @@
 package softuni.exam.domain.entities;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "teams")
 public class Team extends BaseEntity {
-    private String name;
-    private Picture picture;
+  private String name;
+  private Picture picture;
+  private List<Player> players;
 
   public Team() {
+  }
+
+  @OneToMany(mappedBy = "team")
+  public List<Player> getPlayers() {
+    return players;
   }
 
   @Column(unique = true, nullable = false)
@@ -32,6 +36,11 @@ public class Team extends BaseEntity {
 
   public Team setPicture(Picture picture) {
     this.picture = picture;
+    return this;
+  }
+
+  public Team setPlayers(List<Player> players) {
+    this.players = players;
     return this;
   }
 }

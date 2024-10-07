f0e12ef59f189a118fcde5e7d045a96632242925
wolfie <yavorhristozov@gmail.com>
2024-10-07 14:09:34 +0300

    Update PictureRepository.java

--- 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PictureRepository.java
+++ 12. Exam practice/Java DB Advanced Exam - 28 Jul 2019/skeleton/src/main/java/softuni/exam/repository/PictureRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softuni.exam.domain.entities.Picture;
 
+import java.util.Optional;
+
 @Repository
 public interface PictureRepository extends JpaRepository<Picture,Long> {
+
+  Optional<Picture> findByUrl(String url);
 
 }
